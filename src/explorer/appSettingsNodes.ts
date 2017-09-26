633575bfb9de5ab7c73bd2789ec6d02a0404c004
Jay Wang <jiewan@microsoft.com>
2017-09-26 11:30:05 -0700

    Address PR comments

--- src/explorer/appSettingsNodes.ts
+++ src/explorer/appSettingsNodes.ts
@@ -47,8 +47,23 @@ export class AppSettingsNode extends NodeBase {
     }
 
     async getChildren(): Promise<NodeBase[]> {
-        this._settings = await this.getSettings();
-        return this.generateTreeItems(this._settings);
+        const webApps = this.WebSiteManagementClient.webApps;
+        const children: AppSettingNode[] = [];
+        this._settings = this._isSlot ? await webApps.listApplicationSettingsSlot(this.site.resourceGroup, this._siteName, this._slotName) : 
+            await webApps.listApplicationSettings(this.site.resourceGroup, this._siteName);
+
+        if (this._settings.properties) {
+            for (let key in this._settings.properties) {
+                children.push(new AppSettingNode(
+                    key,
+                    this._settings.properties[key],
+                    this.getTreeDataProvider(),
+                    this
+                ));
+            }
+        }
+
+        return children.sort((a, b) => a.label.localeCompare(b.label));
     }
 
     async editSettingItem(oldKey: string, newKey: string, value: string): Promise<void> {
@@ -112,34 +127,11 @@ export class AppSettingsNode extends NodeBase {
         return this.getTreeDataProvider<AppServiceDataProvider>().azureAccount;
     }
 
-    private getSettings(): Promise<WebSiteModels.StringDictionary> {
-        const webApps = this.WebSiteManagementClient.webApps;
-        return this._isSlot ? webApps.listApplicationSettingsSlot(this.site.resourceGroup, this._siteName, this._slotName) : 
-            webApps.listApplicationSettings(this.site.resourceGroup, this._siteName);
-    }
-
     private applySettings(): Promise<WebSiteModels.StringDictionary> {
         const webApps = this.WebSiteManagementClient.webApps;
         const updateTask = this._isSlot ? webApps.updateApplicationSettingsSlot(this.site.resourceGroup, this._siteName, this._settings, this._slotName) :
             webApps.updateApplicationSettings(this.site.resourceGroup, this._siteName, this._settings);
         return updateTask;
-    }
-
-    private generateTreeItems(settings: WebSiteModels.StringDictionary): AppSettingNode[] {
-        const children: AppSettingNode[] = [];
-
-        if (settings.properties) {
-            for (let key in settings.properties) {
-                children.push(new AppSettingNode(
-                    key,
-                    settings.properties[key],
-                    this.getTreeDataProvider(),
-                    this
-                ));
-            }
-        }
-
-        return children.sort((a, b) => a.label.localeCompare(b.label));
     }
 }
 
@@ -169,7 +161,7 @@ export class AppSettingNode extends NodeBase {
             ignoreFocusOut: true,
             prompt: 'Enter setting key',
             value: this.key,
-            validateInput: v => (<AppSettingsNode>this.getParentNode()).validateNewKeyInput(v, oldKey)
+            validateInput: v => this.getParentNode<AppSettingsNode>().validateNewKeyInput(v, oldKey)
         });
 
         if (!newKey) {
@@ -186,7 +178,7 @@ export class AppSettingNode extends NodeBase {
         this.value = newValue;
         this.label = `${this.key} : ${this.value}`;
 
-        await (<AppSettingsNode>this.getParentNode()).editSettingItem(oldKey, newKey, newValue);
+        await this.getParentNode<AppSettingsNode>().editSettingItem(oldKey, newKey, newValue);
 
         // Ideally we only need to refresh the current item, but because of this https://github.com/Microsoft/vscode/issues/34789,
         // have to use workaround for now.
@@ -199,7 +191,7 @@ export class AppSettingNode extends NodeBase {
         const result = await vscode.window.showWarningMessage(`Are you sure you want to delete setting "${this.key}"?`, okayAction);
 
         if (result === okayAction) {
-            await (<AppSettingsNode>this.getParentNode()).deleteSettingItem(this.key);
+            await this.getParentNode<AppSettingsNode>().deleteSettingItem(this.key);
             this.getTreeDataProvider<AppServiceDataProvider>().refresh(this.getParentNode());
         }
     }
