e91e5d2509beecb4e727612e040d895bd0c84df5
nturinski <naturins@microsoft.com>
2017-09-27 16:52:06 -0700

    Fixes some requested changes
    Wraps git actions in try/catch
    Attempts regular push.  If fails, prompts user to force push.
    Removes commit before pushing.  Could add prompt to ask for commit?

--- src/explorer/appServiceNode.ts
+++ src/explorer/appServiceNode.ts
@@ -48,7 +48,7 @@ export class AppServiceNode extends SiteNodeBase {
         // https://github.com/Microsoft/vscode-azureappservice/issues/45
         return [
             new DeploymentSlotsNode(this.site, this.subscription, treeDataProvider, this),
-            // new FilesNode('Files', '/site/wwwroot', this.site, this.subscription),
+            // new FilesNode('Files', '/site/wwwroot', this.site, this.subscription, treeDataProvider, this),
             // new FilesNode('Log Files', '/LogFiles', this.site, this.subscription),
             new WebJobsNode(this.site, this.subscription, treeDataProvider, this),
             new AppSettingsNode(this.site, this.subscription, treeDataProvider, this)
@@ -67,7 +67,64 @@ export class AppServiceNode extends SiteNodeBase {
 
     async restart(): Promise<void> {
         await this.stop();
-        await this.start();
+        return this.start();
+    }
+
+    private get azureAccount(): AzureAccountWrapper {
+        return this.getTreeDataProvider<AppServiceDataProvider>().azureAccount;
+    }
+
+    async localGitDeploy(azureAccount): Promise<boolean> {
+        const publishCredentials = await this.getWebSiteManagementClient(azureAccount).webApps.listPublishingCredentials(this.site.resourceGroup, this.site.name);
+        const config = await this.getWebSiteManagementClient(azureAccount).webApps.getConfiguration(this.site.resourceGroup, this.site.name);
+        const oldDeployment = await this.getWebSiteManagementClient(azureAccount).webApps.listDeployments(this.site.resourceGroup, this.site.name);
+        if (config.scmType !== 'LocalGit') {
+            let input = await window.showErrorMessage(`Local Git Deployment is not set up. Set it up in the Azure Portal.`, `Go to Portal`)
+            if (input === 'Go to Portal') {
+                this.openInPortal();
+            }
+            return;
+        }
+
+        const username = publishCredentials.publishingUserName;
+        const password = publishCredentials.publishingPassword;
+        const repo = `${this.site.enabledHostNames[1]}:443/${this.site.repositorySiteName}.git`;
+        const remote = `https://${username}:${password}@${repo}`;
+
+        let git = require('simple-git/promise')(workspace.rootPath);
+
+        try {
+            await git.init()
+            await git.push(remote, 'master')
+        }
+        catch (err) {
+            if (err.message.indexOf('spawn git ENOENT') >= 0) {
+                let input = await window.showErrorMessage(`Git must be installed to use Local Git Deploy.`, `Install`)
+                if (input === 'Install') {
+                    opn(`https://git-scm.com/downloads`);
+                }
+                return;
+            } else if (err.message.indexOf('error: failed to push') >= 0) {
+                let input = await window.showErrorMessage(`Push rejected due to Git history diverging. Force push?`, `Yes`)
+                if (input === 'Yes') {
+                    await git.push(['-f', remote, 'master']);
+                } else {
+                    return;
+                }
+            }
+        }
+
+        const newDeployment = await this.getWebSiteManagementClient(azureAccount).webApps.listDeployments(this.site.resourceGroup, this.site.name);
+        if (newDeployment[0].deploymentId === oldDeployment[0].deploymentId) {
+            await window.showWarningMessage(`Local Git repo is current with "${repo}".`);
+            return;
+        }
+        return true;
+
+    }
+
+    private getWebSiteManagementClient(azureAccount: AzureAccountWrapper) {
+        return new WebSiteManagementClient(azureAccount.getCredentialByTenantId(this.subscription.tenantId), this.subscription.subscriptionId);
     }
 }
 
