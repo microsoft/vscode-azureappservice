3141f5059194f362d1e58983be80231262db065c
nturinski <naturins@microsoft.com>
2017-11-30 16:53:17 -0800

    Remove unused function

--- src/explorer/SiteTreeItem.ts
+++ src/explorer/SiteTreeItem.ts
@@ -109,19 +109,6 @@ export abstract class SiteTreeItem implements IAzureParentTreeItem {
         }
     }
 
-    public async localGitDeploy(client: WebSiteManagementClient): Promise<void> {
-        const fsWorkspaceFolder = await util.showWorkspaceFoldersQuickPick('Select the folder to Local Git deploy.');
-        try {
-            await this.siteWrapper.localGitDeploy(fsWorkspaceFolder.uri.fsPath, client, getOutputChannel());
-        } catch (err) {
-            if (err instanceof UserCancelledError) {
-                throw err;
-            }
-            const appServicePlan = await getAppServicePlan(this.site, client);
-            throw new SiteActionError(err, appServicePlan.sku.size);
-        }
-    }
-
     public async editScmType(client: WebSiteManagementClient): Promise<string> {
         return await this.siteWrapper.editScmType(client);
     }
