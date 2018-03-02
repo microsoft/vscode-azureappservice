f0d3acbea90f26565bd346c67503e20b55e74a1e
nturinski <naturins@microsoft.com>
2018-03-02 10:59:58 -0800

    Comment

--- src/extension.ts
+++ src/extension.ts
@@ -82,6 +82,7 @@ export function activate(context: vscode.ExtensionContext): void {
         }
 
         node.treeItem.contextValue === 'deploymentSlot' ? node.openInPortal(node.treeItem.id) : node.openInPortal();
+        // the deep link for slots does not follow the conventional pattern of including its parent in the path name so this is how we extract the slot's id
     });
     actionHandler.registerCommand('appService.Start', async (node: IAzureNode<SiteTreeItem>) => {
         if (!node) {
