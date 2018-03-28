071776540099f48138db6a99cafb74288aded14a
Eric Jizba <erijiz@microsoft.com>
2018-03-28 17:51:57 +0000

    Fix open in portal for children of slots (#363)

--- src/extension.ts
+++ src/extension.ts
@@ -85,7 +85,7 @@ export function activate(context: vscode.ExtensionContext): void {
             node = <IAzureNode<WebAppTreeItem>>await tree.showNodePicker(WebAppTreeItem.contextValue);
         }
 
-        node.treeItem.contextValue === 'deploymentSlot' ? node.openInPortal(node.treeItem.id) : node.openInPortal();
+        node.treeItem.contextValue === DeploymentSlotsTreeItem.contextValue ? node.openInPortal(`${node.parent.id}/deploymentSlots`) : node.openInPortal();
         // the deep link for slots does not follow the conventional pattern of including its parent in the path name so this is how we extract the slot's id
     });
     actionHandler.registerCommand('appService.Start', async (node: IAzureNode<SiteTreeItem>) => {
