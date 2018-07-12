240f6ccc09918c7dfdef4f5dfee5b2a9bd7ac74c
nturinski <naturins@microsoft.com>
2018-07-11 18:59:43 -0700

    Move onNodeCreatedFromQuickPick logic

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -31,110 +31,107 @@ export async function deploy(context: IActionContext, target?: vscode.Uri | IAzu
     let defaultWebAppToDeploy: string | undefined;
     let workspaceConfig: vscode.WorkspaceConfiguration;
     context.properties.deployedWithConfigs = 'false';
-    const onNodeCreatedFromQuickPickDisposable: vscode.Disposable = ext.tree.onNodeCreate((newNode: IAzureNode<SiteTreeItem>) => {
-        // event is fired from azure-extensionui if node was created during deployment
-        newNodes.push(newNode);
-    });
 
-    try {
-        if (target instanceof vscode.Uri) {
-            fsPath = target.fsPath;
-            context.properties.deploymentEntryPoint = 'fileExplorerContextMenu';
-        } else {
-            context.properties.deploymentEntryPoint = target ? 'webAppContextMenu' : 'deployButton';
-            node = target;
-        }
-
-        // only use the defaultWebAppToDeploy is there is only one workspace opened
-        if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length === 1) {
-            currentWorkspace = vscode.workspace.workspaceFolders[0];
-            workspaceConfig = vscode.workspace.getConfiguration(constants.extensionPrefix, currentWorkspace.uri);
-            defaultWebAppToDeploy = workspaceConfig.get(constants.configurationSettings.defaultWebAppToDeploy);
-            if (defaultWebAppToDeploy && defaultWebAppToDeploy !== constants.none) {
-                const defaultSubpath: string | undefined = workspaceConfig.get(constants.configurationSettings.deploySubpath);
-                const defaultDeployPath: string = defaultSubpath ? join(currentWorkspace.uri.fsPath, defaultSubpath) : currentWorkspace.uri.fsPath;
-                const defaultPathExists: boolean = await pathExists(defaultDeployPath);
-                const defaultNode: IAzureNode<IAzureTreeItem> | undefined = await ext.tree.findNode(defaultWebAppToDeploy); // resolves to undefined if app can't be found
-                if (defaultPathExists && (!fsPath || isPathEqual(fsPath, defaultDeployPath)) &&
-                    defaultNode && (!node || node.id === defaultNode.id)) {
-                    fsPath = defaultDeployPath;
-                    node = <IAzureNode<SiteTreeItem>>defaultNode;
-                    context.properties.deployedWithConfigs = 'true';
-                } else {
-                    // if defaultPath or defaultNode cannot be found or there was a mismatch, delete old settings and prompt to save next deployment
-                    workspaceConfig.update(constants.configurationSettings.defaultWebAppToDeploy, undefined);
-                }
-            }
-        }
-
-        if (!node) {
-            try {
-                node = <IAzureNode<SiteTreeItem>>await ext.tree.showNodePicker(WebAppTreeItem.contextValue);
-
-            } catch (err2) {
-                if (parseError(err2).isUserCancelledError) {
-                    context.properties.cancelStep = `showNodePicker:${WebAppTreeItem.contextValue}`;
-                }
-                throw err2;
-            }
-        }
-
-        if (newNodes.length > 0) {
-            for (const newApp of newNodes) {
-                if (newApp.id === node.id) {
-                    // if the node selected for deployment is the same newly created nodes, stifle the confirmDeployment dialog
-                    confirmDeployment = false;
-                    newApp.treeItem.client.getSiteConfig().then(
-                        (createdAppConfig: SiteConfigResource) => {
-                            context.properties.linuxFxVersion = createdAppConfig.linuxFxVersion ? createdAppConfig.linuxFxVersion : 'undefined';
-                            context.properties.createdFromDeploy = 'true';
-                        },
-                        () => {
-                            // ignore
-                        });
-                }
-            }
-        }
-
-        const correlationId = getRandomHexString(10);
-        context.properties.correlationId = correlationId;
-        const siteConfig: WebSiteModels.SiteConfigResource = await node.treeItem.client.getSiteConfig();
-
-        if (!fsPath) {
-            if (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.toLowerCase().startsWith(constants.runtimes.tomcat)) {
-                fsPath = await showWarQuickPick('Select the war file to deploy...', context.properties);
-            } else {
-                fsPath = await util.showWorkspaceFoldersQuickPick("Select the folder to deploy", context.properties, constants.configurationSettings.deploySubpath);
-            }
-        }
-        workspaceConfig = vscode.workspace.getConfiguration(constants.extensionPrefix, vscode.Uri.file(fsPath));
-        if (workspaceConfig.get(constants.configurationSettings.showBuildDuringDeployPrompt)) {
-            if (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.startsWith(constants.runtimes.node) && siteConfig.scmType === 'None' && !(await pathExists(path.join(fsPath, constants.deploymentFileName)))) {
-                // check if web app has node runtime, is being zipdeployed, and if there is no .deployment file
-                // tslint:disable-next-line:no-unsafe-any
-                await node.treeItem.enableScmDoBuildDuringDeploy(fsPath, constants.runtimes[siteConfig.linuxFxVersion.substring(0, siteConfig.linuxFxVersion.indexOf('|'))], context.properties);
-            }
-        }
-        if (!defaultWebAppToDeploy && currentWorkspace && (isPathEqual(currentWorkspace.uri.fsPath, fsPath) || isSubpath(currentWorkspace.uri.fsPath, fsPath))) {
-            // tslint:disable-next-line:no-floating-promises
-            node.treeItem.promptToSaveDeployDefaults(node, currentWorkspace.uri.fsPath, fsPath, context.properties);
-        }
-        cancelWebsiteValidation(node.treeItem);
-        await node.runWithTemporaryDescription("Deploying...", async () => {
-            // tslint:disable-next-line:no-non-null-assertion
-            await appservice.deploy(node!.treeItem.client, <string>fsPath, constants.extensionPrefix, confirmDeployment, context.properties);
-        });
-        // Don't wait
-        validateWebSite(correlationId, node.treeItem).then(
-            () => {
-                // ignore
-            },
-            () => {
-                // ignore
-            });
-    } finally {
-        onNodeCreatedFromQuickPickDisposable.dispose();
+    if (target instanceof vscode.Uri) {
+        fsPath = target.fsPath;
+        context.properties.deploymentEntryPoint = 'fileExplorerContextMenu';
+    } else {
+        context.properties.deploymentEntryPoint = target ? 'webAppContextMenu' : 'deployButton';
+        node = target;
     }
+
+    // only use the defaultWebAppToDeploy is there is only one workspace opened
+    if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length === 1) {
+        currentWorkspace = vscode.workspace.workspaceFolders[0];
+        workspaceConfig = vscode.workspace.getConfiguration(constants.extensionPrefix, currentWorkspace.uri);
+        defaultWebAppToDeploy = workspaceConfig.get(constants.configurationSettings.defaultWebAppToDeploy);
+        if (defaultWebAppToDeploy && defaultWebAppToDeploy !== constants.none) {
+            const defaultSubpath: string | undefined = workspaceConfig.get(constants.configurationSettings.deploySubpath);
+            const defaultDeployPath: string = defaultSubpath ? join(currentWorkspace.uri.fsPath, defaultSubpath) : currentWorkspace.uri.fsPath;
+            const defaultPathExists: boolean = await pathExists(defaultDeployPath);
+            const defaultNode: IAzureNode<IAzureTreeItem> | undefined = await ext.tree.findNode(defaultWebAppToDeploy); // resolves to undefined if app can't be found
+            if (defaultPathExists && (!fsPath || isPathEqual(fsPath, defaultDeployPath)) &&
+                defaultNode && (!node || node.id === defaultNode.id)) {
+                fsPath = defaultDeployPath;
+                node = <IAzureNode<SiteTreeItem>>defaultNode;
+                context.properties.deployedWithConfigs = 'true';
+            } else {
+                // if defaultPath or defaultNode cannot be found or there was a mismatch, delete old settings and prompt to save next deployment
+                workspaceConfig.update(constants.configurationSettings.defaultWebAppToDeploy, undefined);
+            }
+        }
+    }
+
+    if (!node) {
+        const onNodeCreatedFromQuickPickDisposable: vscode.Disposable = ext.tree.onNodeCreate((newNode: IAzureNode<SiteTreeItem>) => {
+            // event is fired from azure-extensionui if node was created during deployment
+            newNodes.push(newNode);
+        });
+        try {
+            node = <IAzureNode<SiteTreeItem>>await ext.tree.showNodePicker(WebAppTreeItem.contextValue);
+        } catch (err2) {
+            if (parseError(err2).isUserCancelledError) {
+                context.properties.cancelStep = `showNodePicker:${WebAppTreeItem.contextValue}`;
+            }
+            throw err2;
+        } finally {
+            onNodeCreatedFromQuickPickDisposable.dispose();
+        }
+    }
+
+    if (newNodes.length > 0) {
+        for (const newApp of newNodes) {
+            if (newApp.id === node.id) {
+                // if the node selected for deployment is the same newly created nodes, stifle the confirmDeployment dialog
+                confirmDeployment = false;
+                newApp.treeItem.client.getSiteConfig().then(
+                    (createdAppConfig: SiteConfigResource) => {
+                        context.properties.linuxFxVersion = createdAppConfig.linuxFxVersion ? createdAppConfig.linuxFxVersion : 'undefined';
+                        context.properties.createdFromDeploy = 'true';
+                    },
+                    () => {
+                        // ignore
+                    });
+            }
+        }
+    }
+
+    const correlationId = getRandomHexString(10);
+    context.properties.correlationId = correlationId;
+    const siteConfig: WebSiteModels.SiteConfigResource = await node.treeItem.client.getSiteConfig();
+
+    if (!fsPath) {
+        if (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.toLowerCase().startsWith(constants.runtimes.tomcat)) {
+            fsPath = await showWarQuickPick('Select the war file to deploy...', context.properties);
+        } else {
+            fsPath = await util.showWorkspaceFoldersQuickPick("Select the folder to deploy", context.properties, constants.configurationSettings.deploySubpath);
+        }
+    }
+    workspaceConfig = vscode.workspace.getConfiguration(constants.extensionPrefix, vscode.Uri.file(fsPath));
+    if (workspaceConfig.get(constants.configurationSettings.showBuildDuringDeployPrompt)) {
+        if (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.startsWith(constants.runtimes.node) && siteConfig.scmType === 'None' && !(await pathExists(path.join(fsPath, constants.deploymentFileName)))) {
+            // check if web app has node runtime, is being zipdeployed, and if there is no .deployment file
+            // tslint:disable-next-line:no-unsafe-any
+            await node.treeItem.enableScmDoBuildDuringDeploy(fsPath, constants.runtimes[siteConfig.linuxFxVersion.substring(0, siteConfig.linuxFxVersion.indexOf('|'))], context.properties);
+        }
+    }
+    if (!defaultWebAppToDeploy && currentWorkspace && (isPathEqual(currentWorkspace.uri.fsPath, fsPath) || isSubpath(currentWorkspace.uri.fsPath, fsPath))) {
+        // tslint:disable-next-line:no-floating-promises
+        node.treeItem.promptToSaveDeployDefaults(node, currentWorkspace.uri.fsPath, fsPath, context.properties);
+    }
+    cancelWebsiteValidation(node.treeItem);
+    await node.runWithTemporaryDescription("Deploying...", async () => {
+        // tslint:disable-next-line:no-non-null-assertion
+        await appservice.deploy(node!.treeItem.client, <string>fsPath, constants.extensionPrefix, confirmDeployment, context.properties);
+    });
+    // Don't wait
+    validateWebSite(correlationId, node.treeItem).then(
+        () => {
+            // ignore
+        },
+        () => {
+            // ignore
+        });
 }
 
 function getRandomHexString(length: number): string {
