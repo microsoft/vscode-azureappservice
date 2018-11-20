d758f9fd843af834ea2e19433bc19d63e36d1afb
Eric Jizba <erijiz@microsoft.com>
2018-11-20 16:25:06 -0600

    Add icon to LogStreamTreeItem and use Title Case (#721)

--- src/explorer/LogStreamTreeItem.ts
+++ src/explorer/LogStreamTreeItem.ts
@@ -3,20 +3,26 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as path from 'path';
 import { ISiteTreeRoot } from 'vscode-azureappservice';
 import { AzureTreeItem } from 'vscode-azureextensionui';
 import { FolderTreeItem } from './FolderTreeItem';
-
-const connectLogStream: string = 'Connect to log stream...';
 
 export class LogStreamTreeItem extends AzureTreeItem<ISiteTreeRoot> {
     public static contextValue: string = 'logstream';
     public readonly contextValue: string = LogStreamTreeItem.contextValue;
     public parent: FolderTreeItem;
     public commandId: string = 'appService.OpenLogStream';
-    public label: string = connectLogStream;
+    public label: string = 'Connect to Log Stream...';
 
     constructor(parent: FolderTreeItem) {
         super(parent);
+    }
+
+    public get iconPath(): { light: string, dark: string } {
+        return {
+            light: path.join(__filename, '..', '..', '..', '..', 'resources', 'light', 'StartLog.svg'),
+            dark: path.join(__filename, '..', '..', '..', '..', 'resources', 'dark', 'StartLog.svg')
+        };
     }
 }
