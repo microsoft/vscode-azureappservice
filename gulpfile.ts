30e1f2b098f5c570cb19bf681f8c1e4108c200b2
Eric Jizba <erijiz@microsoft.com>
2018-12-06 14:17:47 -0800

    Update to gulp 4 and clean up pipelines (#772)

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,34 +3,40 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const gulp = require('gulp');
-const decompress = require('gulp-decompress');
-const download = require('gulp-download');
-const path = require('path');
-const os = require('os');
-const cp = require('child_process');
-const glob = require('glob');
+// tslint:disable:no-console
+// tslint:disable:no-implicit-dependencies (this allows the use of dev dependencies)
 
-gulp.task('test', ['install-azure-account'], (cb) => {
+// Grandfathered in
+// tslint:disable:typedef
+// tslint:disable:no-unsafe-any
+
+import * as cp from 'child_process';
+import * as glob from 'glob';
+import * as gulp from 'gulp';
+import * as decompress from 'gulp-decompress';
+import * as download from 'gulp-download';
+import * as os from 'os';
+import * as path from 'path';
+
+function test() {
     const env = process.env;
-    env.DEBUGTELEMETRY = 1;
+    env.DEBUGTELEMETRY = '1';
+    env.MOCHA_timeout = String(10 * 1000);
     env.MOCHA_reporter = 'mocha-junit-reporter';
     env.MOCHA_FILE = path.join(__dirname, 'test-results.xml');
-    const cmd = cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-    cmd.on('close', (code) => {
-        cb(code);
-    });
-});
+    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
+}
 
 /**
  * Installs the azure account extension before running tests (otherwise our extension would fail to activate)
  * NOTE: The version isn't super important since we don't actually use the account extension in tests
  */
-gulp.task('install-azure-account', () => {
+function installAzureAccount() {
     const version = '0.4.3';
     const extensionPath = path.join(os.homedir(), `.vscode/extensions/ms-vscode.azure-account-${version}`);
     const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
     if (existingExtensions.length === 0) {
+        // tslint:disable-next-line:no-http-string
         return download(`http://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/ms-vscode/extension/azure-account/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`)
             .pipe(decompress({
                 filter: file => file.path.startsWith('extension/'),
@@ -42,6 +48,8 @@ gulp.task('install-azure-account', () => {
             .pipe(gulp.dest(extensionPath));
     } else {
         console.log('Azure Account extension already installed.');
+        return Promise.resolve();
     }
-});
+}
 
+exports.test = gulp.series(installAzureAccount, test);
