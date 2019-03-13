8a5087d29d064d0ab5619d25511c93be5dcd5b9a
nturinski <naturins@microsoft.com>
2019-03-13 16:55:11 -0700

    View new commit in Github command

--- src/explorer/SiteTreeItem.ts
+++ src/explorer/SiteTreeItem.ts
@@ -18,6 +18,7 @@ import { CosmosDBConnection } from './CosmosDBConnection';
 import { CosmosDBTreeItem } from './CosmosDBTreeItem';
 import { FolderTreeItem } from './FolderTreeItem';
 import { WebJobsTreeItem } from './WebJobsTreeItem';
+import { SiteSourceControl } from 'azure-arm-website/lib/models';
 
 export abstract class SiteTreeItem extends AzureParentTreeItem<ISiteTreeRoot> {
     public readonly abstract contextValue: string;
@@ -81,7 +82,8 @@ export abstract class SiteTreeItem extends AzureParentTreeItem<ISiteTreeRoot> {
 
     public async loadMoreChildrenImpl(_clearCache: boolean): Promise<AzureTreeItem<ISiteTreeRoot>[]> {
         const siteConfig: WebSiteModels.SiteConfig = await this.root.client.getSiteConfig();
-        this.deploymentsNode = new DeploymentsTreeItem(this, siteConfig, 'appService.ConnectToGitHub');
+        const sourceControl: SiteSourceControl = await this.root.client.getSourceControl();
+        this.deploymentsNode = new DeploymentsTreeItem(this, siteConfig, sourceControl, 'appService.ConnectToGitHub');
         return [this.appSettingsNode, this._connectionsNode, this.deploymentsNode, this._folderNode, this._logFolderNode, this._webJobsNode];
     }
 
