d2c639d1618331819d6f44a0e5d044aed8e9c912
nturinski <naturins@microsoft.com>
2019-04-26 14:34:59 -0700

    More configureDeploymentSource

---
+++ src/commands/deployments/editScmType.ts
@@ -0,0 +1,23 @@
+import { SiteTreeItem } from "../../explorer/SiteTreeItem";
+import * as appservice from "vscode-azureappservice";
+import { ext } from "../../extensionVariables";
+import { WebAppTreeItem } from "../../explorer/WebAppTreeItem";
+import { IActionContext, AzureTreeItem } from "vscode-azureextensionui";
+
+export async function editScmType(actionContext: IActionContext, node?: SiteTreeItem | appservice.DeploymentsTreeItem): Promise<void> {
+    if (!node) {
+        node = <SiteTreeItem>await ext.tree.showTreeItemPicker(WebAppTreeItem.contextValue);
+    } else if (node instanceof appservice.DeploymentsTreeItem) {
+        node = <SiteTreeItem>node.parent;
+    }
+
+    await appservice.editScmType(node.root.client, node, actionContext);
+    const children: AzureTreeItem[] = await node.getCachedChildren();
+    const deploymentsTreeItem: AzureTreeItem | undefined = children.find(ti => {
+        return ti.label === appservice.DeploymentsTreeItem.contextValueConnected ||
+            ti.contextValue === appservice.DeploymentsTreeItem.contextValueUnconnected
+    });
+
+    // if we couldn't find the deployments treeItem, just refresh the parent
+    deploymentsTreeItem ? await deploymentsTreeItem.refresh() : await node.refresh();
+}
