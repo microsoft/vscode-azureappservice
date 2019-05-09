79eb93f9e438d0cdde90c3a014263521d26c6a87
Nathan <naturins@microsoft.com>
2019-05-09 17:19:45 -0500

    * Use SiteTreeItem as target rather than Deployments for editScmType (#978)
    * Use SiteTreeItem as target rather than Deployments

--- src/commands/deployments/editScmType.ts
+++ src/commands/deployments/editScmType.ts
@@ -7,15 +7,19 @@ import { DeploymentsTreeItem } from "vscode-azureappservice";
 import * as appservice from "vscode-azureappservice";
 import { IActionContext } from "vscode-azureextensionui";
 import { SiteTreeItem } from "../../explorer/SiteTreeItem";
+import { WebAppTreeItem } from "../../explorer/WebAppTreeItem";
 import { ext } from "../../extensionVariables";
 
 export async function editScmType(actionContext: IActionContext, node?: SiteTreeItem | DeploymentsTreeItem): Promise<void> {
     if (!node) {
-        node = <DeploymentsTreeItem>await ext.tree.showTreeItemPicker([DeploymentsTreeItem.contextValueConnected, DeploymentsTreeItem.contextValueUnconnected]);
-    } else if (node instanceof SiteTreeItem) {
-        node = <DeploymentsTreeItem>node.deploymentsNode;
+        node = <SiteTreeItem>await ext.tree.showTreeItemPicker(WebAppTreeItem.contextValue);
+    } else if (node instanceof DeploymentsTreeItem) {
+        node = <SiteTreeItem>node.parent;
     }
 
-    await appservice.editScmType(node.root.client, node.parent, actionContext);
-    await node.refresh();
+    await appservice.editScmType(node.root.client, node, actionContext);
+
+    if (node.deploymentsNode) {
+        await node.deploymentsNode.refresh();
+    }
 }
