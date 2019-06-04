b7140f568ace113a6bba1ac5cd58cedf1776ebef
unknown <AzureTest@vwuzhai002.redmond.corp.microsoft.com>
2019-06-03 23:19:53 -0700

    Update code according to the comments

--- test/createAzureResource.test.ts
+++ test/createAzureResource.test.ts
@@ -17,9 +17,8 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
     const testAccount: TestAzureAccount = new TestAzureAccount();
     let oldAdvancedCreationSetting: boolean | undefined;
     const regExpLTS: RegExp = /LTS/g;
-    const resourceName1: string = getRandomHexString().toLowerCase();
+    const resourceName: string = getRandomHexString().toLowerCase();
     let webSiteClient: WebSiteManagementClient;
-    const resourceGroupName: string = 'appsvc_rg_windows_centralus';
 
     suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
         if (!longRunningTestsEnabled) {
@@ -54,59 +53,53 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
     });
 
     test('Create and Delete New Web App (Advanced)', async () => {
-        const resourceName2: string = getRandomHexString().toLowerCase();
         await vscode.workspace.getConfiguration(constants.extensionPrefix).update('advancedCreation', true, vscode.ConfigurationTarget.Global);
-        const testInputs: (string | RegExp)[] = [resourceName2, '$(plus) Create new resource group', resourceName2, 'Linux', regExpLTS, '$(plus) Create new App Service plan', resourceName2, 'B1', 'West US'];
+        const testInputs: (string | RegExp)[] = [resourceName, '$(plus) Create new resource group', resourceName, 'Linux', regExpLTS, '$(plus) Create new App Service plan', resourceName, 'B1', 'West US'];
         ext.ui = new TestUserInput(testInputs);
 
-        resourceGroupsToDelete.push(resourceName2);
+        resourceGroupsToDelete.push(resourceName);
         await vscode.commands.executeCommand('appService.CreateWebApp');
-        const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName2, resourceName2);
-        assert.ok(createdApp);
-
-        ext.ui = new TestUserInput([resourceName2, DialogResponses.deleteResponse.title, DialogResponses.yes.title]);
-        await vscode.commands.executeCommand('appService.Delete');
-        const deletedApp: WebSiteManagementModels.Site | undefined = await webSiteClient.webApps.get(resourceName2, resourceName2);
-        assert.ifError(deletedApp); // if app was deleted, get() returns null.  assert.ifError throws if the value passed is not null/undefined
-    });
-
-    test('Create and Delete New Web App (Basic)', async () => {
-        resourceGroupsToDelete.push(resourceGroupName);
-        await vscode.workspace.getConfiguration(constants.extensionPrefix).update('advancedCreation', false, vscode.ConfigurationTarget.Global);
-        ext.ui = new TestUserInput([resourceName1, 'Windows']);
-        await vscode.commands.executeCommand('appService.CreateWebApp');
-        const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.ok(createdApp);
     });
 
     test('Stop Web App', async () => {
         let createdApp: WebSiteManagementModels.Site;
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Running', `Web App state should be 'Running' rather than ${createdApp.state} before stop.`);
-        ext.ui = new TestUserInput([resourceName1]);
+        ext.ui = new TestUserInput([resourceName]);
         await vscode.commands.executeCommand('appService.Stop');
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Stopped', `Web App state should be 'Stopped' rather than ${createdApp.state}.`);
     });
 
     test('Start Web App', async () => {
         let createdApp: WebSiteManagementModels.Site;
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Stopped', `Web App state should be 'Stopped' rather than ${createdApp.state} before start.`);
-        ext.ui = new TestUserInput([resourceName1]);
+        ext.ui = new TestUserInput([resourceName]);
         await vscode.commands.executeCommand('appService.Start');
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Running', `Web App state should be 'Running' rather than ${createdApp.state}.`);
     });
 
     test('Restart Web App', async () => {
         let createdApp: WebSiteManagementModels.Site;
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Running', `Web App state should be 'Running' rather than ${createdApp.state} before restart.`);
-        ext.ui = new TestUserInput([resourceName1, resourceName1]);
+        ext.ui = new TestUserInput([resourceName, resourceName]);
         await vscode.commands.executeCommand('appService.Restart');
-        createdApp = await webSiteClient.webApps.get(resourceGroupName, resourceName1);
+        createdApp = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.equal(createdApp.state, 'Running', `Web App state should be 'Running' rather than ${createdApp.state}.`);
+    });
+
+    test('Delete Web App', async () => {
+        const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName, resourceName);
+        assert.ok(createdApp);
+        ext.ui = new TestUserInput([resourceName, DialogResponses.deleteResponse.title, DialogResponses.yes.title]);
+        await vscode.commands.executeCommand('appService.Delete');
+        const deletedApp: WebSiteManagementModels.Site | undefined = await webSiteClient.webApps.get(resourceName, resourceName);
+        assert.ifError(deletedApp); // if app was deleted, get() returns null.  assert.ifError throws if the value passed is not null/undefined
     });
 });
 
