cd3400d7237964bba34cff53c1d8d26a5e0c3395
nturinski <naturins@microsoft.com>
2019-07-18 15:00:23 -0700

    Update package.json and audit fix

--- src/commands/deploy/getDefaultWebAppToDeploy.ts
+++ src/commands/deploy/getDefaultWebAppToDeploy.ts
@@ -3,20 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { workspace, WorkspaceFolder } from "vscode";
+import { WorkspaceFolder } from "vscode";
 import { configurationSettings, none } from "../../constants";
 import { WebAppTreeItem } from "../../explorer/WebAppTreeItem";
 import { ext } from '../../extensionVariables';
-import { getContainingWorkspace } from "../../utils/workspace";
-import { getGlobalSetting, getWorkspaceSetting, updateGlobalSetting, updateWorkspaceSetting } from "../../vsCodeConfig/settings";
+import { getWorkspaceSetting, updateWorkspaceSetting } from "../../vsCodeConfig/settings";
 import { IDeployWizardContext } from "../createWebApp/setAppWizardContextDefault";
 
 export async function getDefaultWebAppToDeploy(context: IDeployWizardContext): Promise<WebAppTreeItem | undefined> {
-    const workspaceFolder: WorkspaceFolder | undefined = context.fsPath ?
-        getContainingWorkspace(context.fsPath) : workspace.workspaceFolders && workspace.workspaceFolders.length === 1 ?
-            workspace.workspaceFolders[0] : undefined;
-
-    const defaultWebAppId: string | undefined = workspaceFolder ? getWorkspaceSetting(configurationSettings.defaultWebAppToDeploy, workspaceFolder.uri.fsPath) : getGlobalSetting(configurationSettings.defaultWebAppToDeploy);
+    const workspaceFolder: WorkspaceFolder | undefined = context.workspace;
+    const defaultWebAppId: string | undefined = getWorkspaceSetting(configurationSettings.defaultWebAppToDeploy, workspaceFolder.uri.fsPath);
 
     if (defaultWebAppId && defaultWebAppId !== none) {
         const defaultWebApp: WebAppTreeItem | undefined = await ext.tree.findTreeItem(defaultWebAppId, context); // resolves to undefined if app can't be found
@@ -26,7 +22,7 @@ export async function getDefaultWebAppToDeploy(context: IDeployWizardContext): P
             return <WebAppTreeItem>defaultWebApp;
         } else {
             // if defaultPath or defaultNode cannot be found or there was a mismatch, delete old settings and prompt to save next deployment
-            workspaceFolder ? updateWorkspaceSetting(configurationSettings.defaultWebAppToDeploy, undefined, workspaceFolder.uri.fsPath) : updateGlobalSetting(configurationSettings.defaultWebAppToDeploy, undefined);
+            await updateWorkspaceSetting(configurationSettings.defaultWebAppToDeploy, undefined, workspaceFolder.uri.fsPath);
         }
     }
 
