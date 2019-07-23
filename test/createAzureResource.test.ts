0a118cf17e0be1c4bf2ad2b1e5d83c1c62fe5aca
unknown <AzureTest@vwuzhai002.redmond.corp.microsoft.com>
2019-07-23 03:14:16 -0700

    Add deploying zip files testcase

--- test/createAzureResource.test.ts
+++ test/createAzureResource.test.ts
@@ -6,15 +6,12 @@
 import * as assert from 'assert';
 import { ResourceManagementClient } from 'azure-arm-resource';
 import { WebSiteManagementClient, WebSiteManagementModels } from 'azure-arm-website';
-import * as fse from 'fs-extra';
 import { IHookCallbackContext, ISuiteCallbackContext } from 'mocha';
-import * as os from 'os';
 import * as path from 'path';
 import * as request from 'request-promise';
 import * as vscode from 'vscode';
 import { AzExtTreeDataProvider, AzureAccountTreeItem, constants, DialogResponses, ext, getRandomHexString, TestAzureAccount, TestUserInput } from '../extension.bundle';
 import { longRunningTestsEnabled } from './global.test';
-import { unzipFileFromUrl } from './unzipFileFromUrl';
 
 // tslint:disable-next-line: max-func-body-length
 suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Promise<void> {
@@ -25,7 +22,6 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
     const regExpLTS: RegExp = /LTS/g;
     const resourceName: string = getRandomHexString().toLowerCase();
     let webSiteClient: WebSiteManagementClient;
-    const testFolderPath: string = path.join(os.tmpdir(), `appServiceTest${getRandomHexString()}`, 'testFolder');
 
     suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
         if (!longRunningTestsEnabled) {
@@ -45,7 +41,6 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
         }
         await vscode.workspace.getConfiguration(constants.extensionPrefix).update('advancedCreation', oldAdvancedCreationSetting, vscode.ConfigurationTarget.Global);
         this.timeout(1200 * 1000);
-        await fse.remove(path.join(testFolderPath, '..'));
         const client: ResourceManagementClient = getResourceManagementClient(testAccount);
         for (const resourceGroup of resourceGroupsToDelete) {
             if (await client.resourceGroups.checkExistence(resourceGroup)) {
@@ -119,27 +114,6 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
         assert.equal(createdApp.scmType, constants.ScmType.None, `Web App scmType's property value should be ${constants.ScmType.None} rather than ${createdApp.scmType}.`);
     });
 
-    test('Create a new windows Web app and deployment to Web App', async () => {
-        const resourceGroupName: string = getRandomHexString();
-        const webAppName: string = getRandomHexString();
-        const AppServicePlan: string = getRandomHexString();
-        const inputs: string[] = [];
-        await fse.ensureDir(testFolderPath);
-        resourceGroupsToDelete.push(resourceGroupName);
-        const uri: string = 'https://codeload.github.com/Azure-Samples/nodejs-docs-hello-world/zip/master';
-        await unzipFileFromUrl(vscode.Uri.parse(uri), 'nodejs-docs-hello-world-master', testFolderPath);
-        await vscode.workspace.getConfiguration(constants.extensionPrefix).update('advancedCreation', true, vscode.ConfigurationTarget.Global);
-        inputs.unshift('Deploy');
-        if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
-            inputs.unshift('$(file-directory) Browse...'); // If the test environment has an open workspace, select the 'Browse...' option
-        }
-        inputs.unshift(testFolderPath, '$(plus) Create new Web App...', webAppName, '$(plus) Create new resource group', resourceGroupName, 'Windows', '$(plus) Create new App Service plan', AppServicePlan, 'S1', 'West US');
-        ext.ui = new TestUserInput(inputs);
-        await vscode.commands.executeCommand('appService.Deploy');
-        const result: string = await getBody(`https://${webAppName}.azurewebsites.net`);
-        assert.equal(result, `Hello World!`, `The result should be "Hello World!" rather than ${result}`);
-    });
-
     test('Delete Web App', async () => {
         const createdApp: WebSiteManagementModels.Site = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.ok(createdApp);
@@ -148,6 +122,37 @@ suite('Create Azure Resources', async function (this: ISuiteCallbackContext): Pr
         const deletedApp: WebSiteManagementModels.Site | undefined = await webSiteClient.webApps.get(resourceName, resourceName);
         assert.ifError(deletedApp); // if app was deleted, get() returns null.  assert.ifError throws if the value passed is not null/undefined
     });
+
+    test('Create a new windows Web app and deploying zips to Web App', async () => {
+        const resourceGroupName: string = getRandomHexString();
+        const webAppName: string = getRandomHexString();
+        const AppServicePlan: string = getRandomHexString();
+        resourceGroupsToDelete.push(resourceGroupName);
+        const testInputs: string[] = [getTestRootZipFile(), '$(plus) Create new Web App...', webAppName, '$(plus) Create new resource group', resourceGroupName, 'Windows', '$(plus) Create new App Service plan', AppServicePlan, 'S1', 'West US', 'Deploy'];
+        await testDeploy(testInputs, webAppName);
+    });
+
+    test('Create a new windows Web app and deploying folders to Web App', async () => {
+        const resourceGroupName: string = getRandomHexString();
+        const webAppName: string = getRandomHexString();
+        const AppServicePlan: string = getRandomHexString();
+        const testInputs: string[] = [];
+        resourceGroupsToDelete.push(resourceGroupName);
+        const projectFolder: string = path.join(__dirname, '../../test/testFolder/nodejs-docs-hello-world-master');
+        testInputs.unshift(projectFolder, '$(plus) Create new Web App...', webAppName, '$(plus) Create new resource group', resourceGroupName, 'Windows', '$(plus) Create new App Service plan', AppServicePlan, 'S1', 'West US', 'Deploy');
+        if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
+            testInputs.unshift('$(file-directory) Browse...'); // If the test environment has an open workspace, select the 'Browse...' option
+        }
+        await testDeploy(testInputs, webAppName);
+    });
+
+    async function testDeploy(createDeployInputs: string[], webAppName: string): Promise<void> {
+        await vscode.workspace.getConfiguration(constants.extensionPrefix).update('advancedCreation', true, vscode.ConfigurationTarget.Global);
+        ext.ui = new TestUserInput(createDeployInputs);
+        await vscode.commands.executeCommand('appService.Deploy');
+        const result: string = await getBody(`https://${webAppName}.azurewebsites.net`);
+        assert.equal(result, `Hello World!`, `The result should be "Hello World!" rather than ${result}`);
+    }
 });
 
 function getWebsiteManagementClient(testAccount: TestAzureAccount): WebSiteManagementClient {
@@ -156,6 +161,32 @@ function getWebsiteManagementClient(testAccount: TestAzureAccount): WebSiteManag
 
 function getResourceManagementClient(testAccount: TestAzureAccount): ResourceManagementClient {
     return new ResourceManagementClient(testAccount.getSubscriptionCredentials(), testAccount.getSubscriptionId());
+}
+
+// The root workspace ".zip" that vscode is opened against for tests
+function getTestRootZipFile(): string {
+    let testRootZipFile: string = '';
+    if (!testRootZipFile) {
+        // We're expecting to be opened against the test/test.code-workspace
+        // workspace.
+        const workspaceFolders: vscode.WorkspaceFolder[] | undefined = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders || workspaceFolders.length === 0) {
+            console.error("No workspace is open.");
+            process.exit(1);
+        } else {
+            if (workspaceFolders.length > 1) {
+                console.error("There are unexpected multiple workspaces open");
+                process.exit(1);
+            }
+            testRootZipFile = workspaceFolders[0].name;
+            console.log(`testRootFolder: ${testRootZipFile}`);
+            if (testRootZipFile !== 'nodejs-docs-hello-world-master.zip') {
+                console.error("vscode is opened against the wrong folder for tests");
+                process.exit(1);
+            }
+        }
+    }
+    return testRootZipFile;
 }
 
 async function getBody(url: string): Promise<string> {
