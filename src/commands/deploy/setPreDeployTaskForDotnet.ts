71c79c9748af61fd4e80e6f343f30a091bbb3367
nturinski <naturins@microsoft.com>
2019-08-22 11:32:17 -0700

    Add tasks and dotnet predeploy

---
+++ src/commands/deploy/setPreDeployTaskForDotnet.ts
@@ -0,0 +1,80 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { SiteConfig } from 'azure-arm-website/lib/models';
+import * as fse from 'fs-extra';
+import * as path from 'path';
+import { Uri } from 'vscode';
+import { ext } from 'vscode-azureappservice/out/src/extensionVariables';
+import * as constants from '../../constants';
+import { findFilesByFileExtension, mapFilesToQuickPickItems } from '../../utils/workspace';
+import { getWorkspaceSetting, updateWorkspaceSetting } from '../../vsCodeConfig/settings';
+import * as tasks from '../../vsCodeConfig/tasks';
+import { IDeployWizardContext } from "./IDeployWizardContext";
+
+export async function setPreDeployTaskForDotnet(context: IDeployWizardContext, siteConfig: SiteConfig): Promise<void> {
+    // don't overwrite preDeploy task if it exists
+    if (!getWorkspaceSetting<boolean>('configurePreDeployTasks', context.workspace.uri.fsPath) || getWorkspaceSetting<boolean>(constants.configurationSettings.preDeployTask, context.workspace.uri.fsPath)) {
+        return;
+    }
+
+    // assume that the csProj is in the root
+    let csProjFsPath: string = context.workspace.uri.fsPath;
+
+    const csprojFile: string | undefined = await tryGetCsprojFile(csProjFsPath);
+
+    if (csprojFile) {
+        csProjFsPath = path.dirname(csProjFsPath);
+    }
+
+    // if we found a csproj file or we know the app is .NET, go ahead and set the tasks and workspace settings
+    if (csprojFile || (siteConfig.linuxFxVersion && siteConfig.linuxFxVersion.toLowerCase().includes('dotnet'))) {
+        // follow the publish output patterns, but leave out targetFramework
+        // use the absolute path so the bits are created in the root, not the subpath
+        const dotnetOutputPath: string = path.join(csProjFsPath, 'bin', 'Debug', 'publish');
+
+        const preDeployTaskSetting: string = 'preDeployTask';
+        await updateWorkspaceSetting(preDeployTaskSetting, 'publish', context.workspace.uri.fsPath);
+        await updateWorkspaceSetting(constants.configurationSettings.deploySubpath, dotnetOutputPath, context.workspace.uri.fsPath);
+
+        const publishCommand: string = `dotnet publish ${csProjFsPath} -o ${dotnetOutputPath}`;
+        const publishTask: tasks.ITask[] = [{
+            label: 'clean',
+            command: `dotnet clean ${csProjFsPath}`,
+            type: 'shell'
+        },
+        {
+            label: 'publish',
+            command: publishCommand,
+            type: 'shell',
+            dependsOn: 'clean'
+        }];
+
+        tasks.updateTasks(context.workspace, publishTask);
+
+    }
+
+    async function tryGetCsprojFile(projectPath: string): Promise<string | undefined> {
+        const projectFiles: string[] = await checkFolderForCsproj(projectPath);
+        // it's a common pattern to have the .csproj file in a subfolder
+        if (projectFiles.length === 0) {
+            for (const folder of await fse.readdir(projectPath)) {
+                if ((await fse.stat(folder)).isDirectory()) {
+                    projectFiles.concat(await checkFolderForCsproj(folder));
+                }
+            }
+        }
+
+        context.telemetry.properties.numOfCsprojFiles = projectFiles.length.toString();
+
+        return projectFiles.length === 1 ? projectFiles[0] : undefined;
+
+        async function checkFolderForCsproj(filePath: string): Promise<string[]> {
+            const files: string[] = await fse.readdir(filePath);
+            return files.filter((f: string) => /\.csproj$/i.test(f));
+        }
+    }
+
+}
