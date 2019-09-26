37a86e1594e9029e266c11446b8eeda3253f7be9
William Lorey <wilorey@microsoft.com>
2019-09-26 10:16:02 -0700

    Address PR comments

--- src/commands/createWebApp/readAzConfig.ts
+++ src/commands/createWebApp/readAzConfig.ts
@@ -7,6 +7,11 @@ import * as fse from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
 
+/**
+ * Takes any number of Azure config properties and returns their configured values
+ * found in `$AZURE_CONFIG_DIR/config`.
+ * Azure CLI configuration docs: https://aka.ms/AA64syh
+ */
 export async function readAzConfig(...properties: AzConfigProperties[]): Promise<AzConfig> {
     const config: AzConfig = {};
     const configPath: string = path.join(os.homedir(), '.azure', 'config');
