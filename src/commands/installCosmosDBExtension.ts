1d667976ff7c4f4cd85b7535a31ef8c431f2515d
nturinski <naturins@microsoft.com>
2019-11-07 15:17:27 -0800

    Add license

--- src/commands/installCosmosDBExtension.ts
+++ src/commands/installCosmosDBExtension.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { commands } from 'vscode';
 import { AzureTreeItem, IActionContext } from 'vscode-azureextensionui';
 import { CosmosDBTreeItem } from '../explorer/CosmosDBTreeItem';
