71ce400b06b847a5a1a157f455722d189f0ac476
nturinski <naturins@microsoft.com>
2019-11-08 16:05:15 -0800

    Remove unneeded check

--- src/commands/installCosmosDBExtension.ts
+++ src/commands/installCosmosDBExtension.ts
@@ -7,19 +7,10 @@ import { commands } from 'vscode';
 import { AzureTreeItem, IActionContext } from 'vscode-azureextensionui';
 import { CosmosDBTreeItem } from '../explorer/CosmosDBTreeItem';
 import { delay } from '../utils/delay';
-import { openUrl } from "../utils/openUrl";
 
 export async function installCosmosDBExtension(context: IActionContext, treeItem: AzureTreeItem): Promise<void> {
-    const commandToRun = 'extension.open';
-    const listOfCommands = await commands.getCommands();
-    if (listOfCommands.find((x: string) => x === commandToRun)) {
-        commands.executeCommand(commandToRun, 'ms-azuretools.vscode-cosmosdb');
-        context.telemetry.properties.viaVsCode = 'true';
-    } else {
-        // leads to https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-cosmosdb
-        await openUrl('https://aka.ms/AA6irqo');
-        context.telemetry.properties.viaVsCode = 'false';
-    }
+    const commandToRun: string = 'extension.open';
+    commands.executeCommand(commandToRun, 'ms-azuretools.vscode-cosmosdb');
 
     // poll to see if the extension was installed for a minute
     const timeoutInSeconds: number = 60;
