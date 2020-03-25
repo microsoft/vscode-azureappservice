aabb917754776a744ce292300575ee4635a85869
Eric Jizba <erijiz@microsoft.com>
2020-03-25 14:18:25 -0700

    Update how we run tests

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as cp from 'child_process';
 import * as fse from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
@@ -20,15 +19,6 @@ async function prepareForWebpack(): Promise<void> {
     await fse.writeFile(mainJsPath, contents);
 }
 
-function test(): cp.ChildProcess {
-    const env = process.env;
-
-    env.DEBUGTELEMETRY = '1';
-    env.CODE_TESTS_WORKSPACE = path.join(__dirname, 'test/test.code-workspace');
-    env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
-    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-}
-
 exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
 exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
-exports.test = gulp.series(gulp_installAzureAccount, test);
+exports.preTest = gulp_installAzureAccount;
