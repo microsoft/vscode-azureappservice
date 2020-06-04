60cd12b366f186e1ec47b59cb34eda5520cc4325
alexweininger <alex.weininger@live.com>
2020-06-04 12:05:10 -0700

    Change error message

--- src/commands/deployments/disconnectRepo.ts
+++ src/commands/deployments/disconnectRepo.ts
@@ -17,6 +17,6 @@ export async function disconnectRepo(context: IActionContext, node?: Deployments
     if (node.parent instanceof SiteTreeItem) {
         await disconnectRepository(context, node.parent.root.client, node.parent.root);
     } else {
-        throw Error(localize('actionNotSupported', 'Action not supported.'));
+        throw Error(localize('notSupported', 'This operation is not supported.'));
     }
 }
