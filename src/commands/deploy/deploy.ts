f356805c97b820a1dd9de72436a07a19b5bf8ca7
alexweininger <alex.weininger@live.com>
2020-06-08 12:41:36 -0700

    Deploy trial apps

--- src/commands/deploy/deploy.ts
+++ src/commands/deploy/deploy.ts
@@ -6,12 +6,17 @@
 import * as WebSiteModels from 'azure-arm-website/lib/models';
 import { pathExists } from 'fs-extra';
 import * as path from 'path';
+import * as git from 'simple-git/promise';
 import * as vscode from 'vscode';
+import { commands, MessageItem, ProgressLocation, window, workspace, WorkspaceFolder } from 'vscode';
 import * as appservice from 'vscode-azureappservice';
-import { IActionContext } from 'vscode-azureextensionui';
+import { callWithTelemetryAndErrorHandling, IActionContext } from 'vscode-azureextensionui';
 import * as constants from '../../constants';
+import { AppServiceDialogResponses } from '../../constants';
 import { SiteTreeItem } from '../../explorer/SiteTreeItem';
+import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
 import { ext } from '../../extensionVariables';
+import { localize } from '../../localize';
 import { javaUtils } from '../../utils/javaUtils';
 import { nonNullValue } from '../../utils/nonNull';
 import { isPathEqual } from '../../utils/pathUtils';
@@ -28,10 +33,19 @@ import { showDeployCompletedMessage } from './showDeployCompletedMessage';
 
 const postDeployCancelTokens: Map<string, vscode.CancellationTokenSource> = new Map();
 
-export async function deploy(context: IActionContext, target?: vscode.Uri | SiteTreeItem, _multiTargets?: (vscode.Uri | SiteTreeItem)[], isTargetNewWebApp: boolean = false): Promise<void> {
+export async function deploy(context: IActionContext, target?: vscode.Uri | SiteTreeItem | TrialAppTreeItem, _multiTargets?: (vscode.Uri | SiteTreeItem)[], isTargetNewWebApp: boolean = false): Promise<void> {
     let webAppSource: WebAppSource | undefined;
     context.telemetry.properties.deployedWithConfigs = 'false';
     let siteConfig: WebSiteModels.SiteConfigResource | undefined;
+
+    if (target instanceof TrialAppTreeItem) {
+        return await deployTrialApp(target);
+    }
+
+    const trialAppTreeItem: TrialAppTreeItem | undefined = await shouldDeployTrialApp(target);
+    if (trialAppTreeItem) {
+        return await deployTrialApp(trialAppTreeItem);
+    }
 
     if (target instanceof SiteTreeItem) {
         webAppSource = WebAppSource.tree;
@@ -114,4 +128,64 @@ export async function deploy(context: IActionContext, target?: vscode.Uri | Site
     // don't wait
     // tslint:disable-next-line: no-floating-promises
     runPostDeployTask(node, correlationId, tokenSource);
+}
+
+async function shouldDeployTrialApp(target: vscode.Uri | SiteTreeItem | undefined): Promise<TrialAppTreeItem | undefined> {
+    const trialAppTreeItem: TrialAppTreeItem | undefined = ext.azureAccountTreeItem.trialAppTreeItem;
+    if (target === undefined && trialAppTreeItem) {
+        const response: vscode.QuickPickItem = await ext.ui.showQuickPick([{ label: 'Deploy to trial app' }, { label: 'Deploy to web app' }], { placeHolder: '' });
+        if (response.label === 'Deploy to trial app') {
+            return trialAppTreeItem;
+        }
+    }
+    return undefined;
+}
+
+async function deployTrialApp(trialAppTreeItem: TrialAppTreeItem): Promise<void> {
+    const trialAppNotFoundError: Error = Error(localize('unableToDeployTrialApp', 'Unable to deploy trial app: Clone trial app source and open folder in VS Code to deploy'));
+    const title: string = localize('deploying', 'Deploying to "{0}"... Check [output window](command:{1}) for status.', trialAppTreeItem.metadata.siteName, `${ext.prefix}.showOutputChannel`);
+
+    const workspaceFolders: WorkspaceFolder[] | undefined = workspace.workspaceFolders;
+    if (workspaceFolders && workspaceFolders.length < 0) {
+        const trialAppPath = workspaceFolders.find((folder: WorkspaceFolder) => {
+            return folder.name === trialAppTreeItem.metadata.siteName;
+        });
+        if (trialAppPath) {
+            await window.withProgress({ location: ProgressLocation.Notification, title }, async () => {
+
+                // the -a flag stages all changes before committing
+                ext.outputChannel.appendLog(localize('committingChanges', 'Committing changes'));
+                await git(trialAppPath.uri.fsPath).commit('Deploy trial app', undefined, { '-a': null });
+                ext.outputChannel.appendLog(localize('pushingToRemote', 'Pushing to deploy changes'));
+                await commands.executeCommand('git.push');
+                // use showDeployCompletedMessage when able
+                const message: string = localize('deployCompleted', 'Deployment to trial app "{0}" completed.', trialAppTreeItem.metadata.siteName);
+                ext.outputChannel.appendLog(message);
+                const browseWebsiteBtn: MessageItem = { title: localize('browseWebsite', 'Browse Website') };
+                const streamLogs: MessageItem = { title: localize('streamLogs', 'Stream Logs') };
+
+                // don't wait
+                window.showInformationMessage(message, browseWebsiteBtn, streamLogs, AppServiceDialogResponses.viewOutput).then(async (result: MessageItem | undefined) => {
+                    await callWithTelemetryAndErrorHandling('postDeploy', async (context: IActionContext) => {
+                        context.telemetry.properties.dialogResult = result?.title;
+                        if (result === AppServiceDialogResponses.viewOutput) {
+                            ext.outputChannel.show();
+                        } else if (result === browseWebsiteBtn) {
+                            await trialAppTreeItem.browse();
+                        }
+                    });
+                });
+            });
+        } else {
+            const clone: MessageItem = { title: 'Clone trial app' };
+            await window.showErrorMessage(trialAppNotFoundError.message, clone).then((value: MessageItem) => {
+                if (value === clone) {
+                    vscode.commands.executeCommand('git.clone', trialAppTreeItem.metadata.gitUrl);
+                }
+            });
+            return;
+        }
+    } else {
+        throw trialAppNotFoundError;
+    }
 }
