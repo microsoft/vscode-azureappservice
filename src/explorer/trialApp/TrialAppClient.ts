5714c98b00b8c83fa32114db8e5eb546f4ce63ce
Alex Weininger <alex.weininger@live.com>
2020-06-12 17:28:49 -0700

    Add AppSettingsTreeItem to trial app children (#1588)
    * Add app settings node
    
    * Use requestUtils
    
    * Simplify changes
    
    * Bump appservice
    
    * Reduce regex length
    
    * Fix properties
    
    * Reduce regex length
    
    * Add explaining comment to udpateAppSettings

--- src/explorer/trialApp/TrialAppClient.ts
+++ src/explorer/trialApp/TrialAppClient.ts
@@ -3,16 +3,18 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import { StringDictionary } from 'azure-arm-website/lib/models';
 import { BasicAuthenticationCredentials, ServiceClientCredentials } from 'ms-rest';
-import { IFilesClient } from 'vscode-azureappservice';
+import { IAppSettingsClient, IFilesClient } from 'vscode-azureappservice';
 import { addExtensionUserAgent } from 'vscode-azureextensionui';
 import KuduClient from 'vscode-azurekudu';
 import { requestUtils } from '../../utils/requestUtils';
 import { ITrialAppMetadata } from './ITrialAppMetadata';
 
-export class TrialAppClient implements IFilesClient {
+export class TrialAppClient implements IAppSettingsClient, IFilesClient {
 
     public isFunctionApp: boolean = false;
+    public isLinux: boolean = true;
     public metadata: ITrialAppMetadata;
 
     private _credentials: ServiceClientCredentials;
@@ -40,7 +42,7 @@ export class TrialAppClient implements IFilesClient {
     }
 
     public get fullName(): string {
-        return this.metadata.hostName;
+        return this.metadata.siteName;
     }
 
     public get id(): string {
@@ -51,17 +53,55 @@ export class TrialAppClient implements IFilesClient {
         return `https://${this.metadata.scmHostName}`;
     }
 
-    public get defaultHostName(): string {
-        return this.metadata.hostName;
-    }
-
     public get defaultHostUrl(): string {
-        return `https://${this.metadata.hostName}`;
+        return `https://${this.metadata.url}`;
     }
 
     public async getKuduClient(): Promise<KuduClient> {
         const kuduClient: KuduClient = new KuduClient(this._credentials, this.kuduUrl);
         addExtensionUserAgent(kuduClient);
         return kuduClient;
+    }
+
+    public async listApplicationSettings(): Promise<StringDictionary> {
+        const kuduClient: KuduClient = await this.getKuduClient();
+        const settings: StringDictionary = {};
+        settings.properties = <{ [name: string]: string }>await kuduClient.settings.getAll();
+        return settings;
+    }
+
+    public async updateApplicationSettings(appSettings: StringDictionary): Promise<StringDictionary> {
+        const currentSettings: StringDictionary = await this.listApplicationSettings();
+
+        /**
+         * We cannot use websiteManagementClient for trial apps since we do not have a subscription. And KuduClient.settings.set was not
+         * working for an unknown reason (and is lacking documentation), so we are making our own https requests.
+         * Since Azure 'merges' the app settings JSON sent in the request we have to make an explicit call to delete the old app setting when renaming.
+         */
+
+        // tslint:disable-next-line:strict-boolean-expressions
+        const properties: { [name: string]: string } = currentSettings.properties || {};
+        await Promise.all(Object.keys(properties).map(async (key: string) => {
+            if (appSettings.properties && appSettings.properties[key] === undefined) {
+                await this.deleteApplicationSetting(appSettings, key);
+            }
+        }));
+
+        const request: requestUtils.Request = await requestUtils.getDefaultRequest(`https://${this.metadata.scmHostName}/api/settings`, this._credentials, 'POST');
+        request.body = JSON.stringify(appSettings.properties);
+        request.headers['Content-Type'] = 'application/json';
+
+        await requestUtils.sendRequest(request);
+        return appSettings;
+    }
+
+    private async deleteApplicationSetting(appSettings: StringDictionary, key: string): Promise<StringDictionary> {
+
+        const deleteRequest: requestUtils.Request = await requestUtils.getDefaultRequest(`https://${this.metadata.scmHostName}/api/settings/${key}`, this._credentials, 'DELETE');
+        deleteRequest.body = JSON.stringify(appSettings.properties);
+        deleteRequest.headers['Content-Type'] = 'application/json';
+
+        await requestUtils.sendRequest<string>(deleteRequest);
+        return appSettings;
     }
 }
