5477cb0a163e05e945960804b7c2ccc6ea4fd26a
alexweininger <alex.weininger@live.com>
2020-06-16 16:14:35 -0700

    Use ISimplifiedSiteClient

--- src/commands/logstream/startStreamingLogs.ts
+++ src/commands/logstream/startStreamingLogs.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as appservice from 'vscode-azureappservice';
-import { IFilesClient } from 'vscode-azureappservice';
+import { ISimplifiedSiteClient } from 'vscode-azureappservice';
 import { IActionContext } from 'vscode-azureextensionui';
 import { SiteTreeItem } from '../../explorer/SiteTreeItem';
 import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
@@ -25,6 +25,6 @@ export async function startStreamingLogs(context: IActionContext, node?: SiteTre
         }
     };
 
-    const client: IFilesClient = (node instanceof TrialAppTreeItem) ? node.client : node.root.client;
+    const client: ISimplifiedSiteClient = (node instanceof TrialAppTreeItem) ? node.client : node.root.client;
     await appservice.startStreamingLogs(client, verifyLoggingEnabled, node.logStreamLabel);
 }
