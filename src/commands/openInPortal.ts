85ed7cb94ff40c995543f0dfe8a24f7883ce8bea
Alex Weininger <alex.weininger@live.com>
2020-06-24 11:55:03 -0700

    Add deployments to trial app children (#1615)
    * Add deployments to trial app children
    
    * Throw error on openInPortal

--- src/commands/openInPortal.ts
+++ src/commands/openInPortal.ts
@@ -6,13 +6,19 @@
 import { DeploymentsTreeItem } from "vscode-azureappservice";
 import { AzExtTreeItem, IActionContext, openInPortal as uiOpenInPortal } from "vscode-azureextensionui";
 import { DeploymentSlotsTreeItem } from "../explorer/DeploymentSlotsTreeItem";
+import { TrialAppDeploymentsTreeItem } from '../explorer/trialApp/TrialAppTreeItem';
 import { WebAppTreeItem } from "../explorer/WebAppTreeItem";
 import { ext } from "../extensionVariables";
+import { localize } from '../localize';
 import { nonNullProp } from "../utils/nonNull";
 
 export async function openInPortal(context: IActionContext, node?: AzExtTreeItem): Promise<void> {
     if (!node) {
         node = await ext.tree.showTreeItemPicker<WebAppTreeItem>(WebAppTreeItem.contextValue, context);
+    }
+
+    if (node instanceof TrialAppDeploymentsTreeItem || node.parent instanceof TrialAppDeploymentsTreeItem) {
+        throw Error(localize('notSupported', 'This operation is not supported.'));
     }
 
     switch (node.contextValue) {
