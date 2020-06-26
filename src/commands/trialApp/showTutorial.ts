984be97d94a94c6f568c454d95eab0d2852874ae
alexweininger <alex.weininger@live.com>
2020-06-26 15:59:29 -0700

    Refactor

--- src/commands/trialApp/showTutorial.ts
+++ src/commands/trialApp/showTutorial.ts
@@ -10,15 +10,15 @@ import { localize } from '../../localize';
 import { installExtension } from '../../utils/installExtension';
 import { ext } from './../../extensionVariables';
 
-export async function showTutorial(_context: IActionContext): Promise<void> {
-    const extensionId: string = 'redhat.vscode-didact';
-
+export async function showTutorial(context: IActionContext): Promise<void> {
     const trialAppNode: TrialAppTreeItem | undefined = ext.azureAccountTreeItem.trialAppNode;
 
     if (trialAppNode) {
+        const extensionId: string = 'redhat.vscode-didact';
         if (await installExtension(extensionId)) {
             const tutorialUri: Uri = Uri.file(ext.context.asAbsolutePath('resources/TrialApp.didact.md'));
             commands.executeCommand('vscode.didact.startDidact', tutorialUri);
+            context.telemetry.properties.installedDidact = 'true';
         }
     } else {
         throw Error(localize('trialAppNotFound', 'Trial app not found.'));
