6dd05feb76a011320f883d5888e3030ba8d557eb
Alex Weininger <alex.weininger@live.com>
2020-06-30 12:33:19 -0700

    Add trialApp property to telemetry on deploy (#1635)

--- src/commands/deploy/deployTrialApp.ts
+++ src/commands/deploy/deployTrialApp.ts
@@ -12,6 +12,7 @@ import { IDeployContext } from './IDeployContext';
 import { showDeployCompletedMessage } from './showDeployCompletedMessage';
 
 export async function deployTrialApp(deployContext: IDeployContext, node: TrialAppTreeItem): Promise<void> {
+    deployContext.telemetry.properties.trialApp = 'true';
     await node.runWithTemporaryDescription("Deploying...", async () => {
         const commit: boolean = deployContext.workspace.name === node.metadata.siteName;
         const title: string = localize('deploying', 'Deploying to "{0}"... Check [output window](command:{1}) for status.', node.client.fullName, `${ext.prefix}.showOutputChannel`);
