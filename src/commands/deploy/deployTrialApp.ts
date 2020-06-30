060473fe6de216d47e9bd8395c82d7124251db0c
Alex Weininger <alex.weininger@live.com>
2020-06-30 13:11:25 -0700

    Delay for 15 seconds when deploying trial app (#1631)
    * Delay for 15 seconds when deploying trial app

--- src/commands/deploy/deployTrialApp.ts
+++ src/commands/deploy/deployTrialApp.ts
@@ -8,6 +8,7 @@ import { localGitDeploy } from 'vscode-azureappservice';
 import { ext } from 'vscode-azureappservice/out/src/extensionVariables';
 import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
 import { localize } from '../../localize';
+import { delay } from '../../utils/delay';
 import { IDeployContext } from './IDeployContext';
 import { showDeployCompletedMessage } from './showDeployCompletedMessage';
 
@@ -18,6 +19,8 @@ export async function deployTrialApp(deployContext: IDeployContext, node: TrialA
         const title: string = localize('deploying', 'Deploying to "{0}"... Check [output window](command:{1}) for status.', node.client.fullName, `${ext.prefix}.showOutputChannel`);
         return await window.withProgress({ location: ProgressLocation.Notification, title }, async () => {
             await localGitDeploy(node.client, { fsPath: deployContext.workspace.uri.fsPath, branch: 'RELEASE', commit: commit }, deployContext);
+            // Delay for 15 seconds to make sure changes are reflected immediately after deploy 'finishes'
+            await delay(15000);
         });
     });
     showDeployCompletedMessage(node);
