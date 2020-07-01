9d7894016e06b62fd1e300eb19efa351b3fe003a
alexweininger <alex.weininger@live.com>
2020-07-01 11:35:53 -0700

    Add modal on removing trial app

--- src/commands/trialApp/removeTrialApp.ts
+++ src/commands/trialApp/removeTrialApp.ts
@@ -3,7 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from 'vscode-azureextensionui';
+import { MessageItem, window } from 'vscode';
+import { DialogResponses, IActionContext } from 'vscode-azureextensionui';
 import { TrialAppLoginSession } from '../../constants';
 import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
 import { ext } from '../../extensionVariables';
@@ -13,7 +14,13 @@ export async function removeTrialApp(context: IActionContext, node?: TrialAppTre
         node = await ext.tree.showTreeItemPicker<TrialAppTreeItem>(TrialAppTreeItem.contextValue, context);
     }
 
-    ext.context.globalState.update(TrialAppLoginSession, undefined);
-    delete ext.azureAccountTreeItem.trialAppNode;
-    await ext.tree.refresh();
+    const message: string = `Are you sure you want to remove Trial app "${node.client.fullName}"?`;
+    const remove: MessageItem = { title: 'Remove' };
+    window.showInformationMessage(message, { modal: true }, remove, DialogResponses.cancel).then(async (response: MessageItem): Promise<void> => {
+        if (response === remove) {
+            ext.context.globalState.update(TrialAppLoginSession, undefined);
+            delete ext.azureAccountTreeItem.trialAppNode;
+            await ext.tree.refresh();
+        }
+    });
 }
