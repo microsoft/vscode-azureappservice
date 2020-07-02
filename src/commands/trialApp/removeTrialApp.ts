bc887e4ade9ba672d6a06cf06fafdcfcec8fe020
Alex Weininger <alex.weininger@live.com>
2020-07-02 10:35:47 -0700

    Add modal on removing trial app (#1649)
    * Add modal on removing trial app

--- src/commands/trialApp/removeTrialApp.ts
+++ src/commands/trialApp/removeTrialApp.ts
@@ -3,15 +3,21 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { MessageItem } from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { TrialAppContext } from '../../constants';
 import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
 import { ext } from '../../extensionVariables';
+import { localize } from '../../localize';
 
 export async function removeTrialApp(context: IActionContext, node?: TrialAppTreeItem): Promise<void> {
     if (!node) {
         node = await ext.tree.showTreeItemPicker<TrialAppTreeItem>(TrialAppTreeItem.contextValue, context);
     }
+
+    const message: string = localize('removeTrialApp', 'Are you sure you want to remove trial app "{0}"?', node.client.fullName);
+    const remove: MessageItem = { title: 'Remove' };
+    await ext.ui.showWarningMessage(message, { modal: true }, remove);
 
     ext.context.globalState.update(TrialAppContext, undefined);
     delete ext.azureAccountTreeItem.trialAppNode;
