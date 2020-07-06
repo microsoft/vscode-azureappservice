e4442ec7eb9c4b6f4b3f95fd9ac5e53d1d8ef1b9
alexweininger <alex.weininger@live.com>
2020-07-06 10:48:24 -0700

    Use globalContext name

--- src/commands/trialApp/removeTrialApp.ts
+++ src/commands/trialApp/removeTrialApp.ts
@@ -6,21 +6,32 @@
 import { MessageItem } from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { TrialAppContext } from '../../constants';
+import { ExpiredTrialAppTreeItem } from '../../explorer/trialApp/ExpiredTrialAppTreeItem';
+import { ITrialAppContext } from '../../explorer/trialApp/ITrialAppContext';
 import { TrialAppTreeItem } from '../../explorer/trialApp/TrialAppTreeItem';
 import { ext } from '../../extensionVariables';
 import { localize } from '../../localize';
 import { addTrialAppTelemetry } from './addTrialAppTelemetry';
 
-export async function removeTrialApp(context: IActionContext, node?: TrialAppTreeItem): Promise<void> {
+export async function removeTrialApp(context: IActionContext, node?: TrialAppTreeItem | ExpiredTrialAppTreeItem): Promise<void> {
     if (!node) {
         node = await ext.tree.showTreeItemPicker<TrialAppTreeItem>(TrialAppTreeItem.contextValue, context);
     }
 
-    const message: string = localize('removeTrialApp', 'Are you sure you want to remove trial app "{0}"?', node.client.fullName);
+    const trialAppContext: ITrialAppContext | undefined = ext.context.globalState.get(TrialAppContext);
+    if (!trialAppContext) {
+        return;
+    }
+
+    const message: string = localize('removeTrialApp', 'Are you sure you want to remove trial app "{0}"?', trialAppContext.name);
     const remove: MessageItem = { title: 'Remove' };
     await ext.ui.showWarningMessage(message, { modal: true }, remove);
 
-    addTrialAppTelemetry(context, node);
+    if (node instanceof TrialAppTreeItem) {
+        addTrialAppTelemetry(context, node);
+    } else {
+        context.telemetry.properties.trialApp = 'true';
+    }
 
     ext.context.globalState.update(TrialAppContext, undefined);
     delete ext.azureAccountTreeItem.trialAppNode;
