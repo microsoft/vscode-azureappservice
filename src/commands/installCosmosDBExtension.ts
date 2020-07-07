20b59a7194ce5de745dad3bb3a47bb07f68fe56c
alexweininger <alex.weininger@live.com>
2020-07-07 16:15:44 -0700

    Remove extra check

--- src/commands/installCosmosDBExtension.ts
+++ src/commands/installCosmosDBExtension.ts
@@ -3,20 +3,17 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { extensions } from 'vscode';
 import { AzureTreeItem, IActionContext } from 'vscode-azureextensionui';
 import { CosmosDBTreeItem } from '../explorer/CosmosDBTreeItem';
 import { installExtension } from '../utils/installExtension';
 
 export async function installCosmosDBExtension(context: IActionContext, treeItem: AzureTreeItem): Promise<void> {
     const extensionId: string = 'ms-azuretools.vscode-cosmosdb';
-    if (!extensions.getExtension(extensionId)) {
-        if (await installExtension(extensionId)) {
-            if (treeItem.parent) {
-                await treeItem.parent.refresh();
-                if ((<CosmosDBTreeItem>treeItem.parent).cosmosDBExtension) {
-                    context.telemetry.properties.installedCosmos = 'true';
-                }
+    if (await installExtension(extensionId)) {
+        if (treeItem.parent) {
+            await treeItem.parent.refresh();
+            if ((<CosmosDBTreeItem>treeItem.parent).cosmosDBExtension) {
+                context.telemetry.properties.installedCosmos = 'true';
             }
         }
     }
