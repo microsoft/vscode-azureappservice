2b34d8a4e0cab16ef6dc72722fccd0275a992196
Eric Jizba <erijiz@microsoft.com>
2020-11-19 13:45:55 -0800

    Add .NET 5 nightly test (#1843)

--- test/nightly/deploy.test.ts
+++ test/nightly/deploy.test.ts
@@ -9,7 +9,7 @@ import * as assert from 'assert';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { tryGetWebApp } from 'vscode-azureappservice';
-import { createGenericClient, DialogResponses, ext, getRandomHexString, IActionContext, WebAppTreeItem } from '../../extension.bundle';
+import { createGenericClient, ext, getRandomHexString, IActionContext, WebAppTreeItem } from '../../extension.bundle';
 import { longRunningTestsEnabled, testUserInput } from '../global.test';
 import { resourceGroupsToDelete, webSiteClient } from './global.resource.test';
 
@@ -19,45 +19,46 @@ interface ITestCase {
      */
     workspaceFolder: string | undefined;
     runtimePrefix: string;
-    runtimeSuffix?: string;
     versions: IVersionInfo[];
 }
 
 interface IVersionInfo {
     version: string;
-    supportedOs: 'Windows' | 'Linux' | 'Both';
+    displayText?: string;
+    supportedAppOs: 'Windows' | 'Linux' | 'Both';
+    appOsToSkip?: 'Windows' | 'Linux';
+    buildMachineOsToSkip?: NodeJS.Platform;
 }
 
 suite('Create Web App and deploy', async function (this: Mocha.Suite): Promise<void> {
-    this.timeout(12 * 60 * 1000);
+    this.timeout(6 * 60 * 1000);
     const planNames: { [os: string]: string } = {};
     const testCases: ITestCase[] = [
         {
             runtimePrefix: 'Node',
-            runtimeSuffix: ' LTS',
             workspaceFolder: 'nodejs-docs-hello-world',
             versions: [
-                { version: '10', supportedOs: 'Linux' },
-                { version: '12', supportedOs: 'Both' },
-                { version: '14', supportedOs: 'Linux' }
+                { version: '10', supportedAppOs: 'Linux', displayText: '10 LTS' },
+                { version: '12', supportedAppOs: 'Both', displayText: '12 LTS' },
+                { version: '14', supportedAppOs: 'Linux', displayText: '14 LTS' }
             ]
         },
         {
-            runtimePrefix: '.NET Core',
-            runtimeSuffix: ' (LTS)',
+            runtimePrefix: '.NET',
             workspaceFolder: undefined,
             versions: [
-                { version: '2.1', supportedOs: 'Both' },
-                { version: '3.1', supportedOs: 'Both' }
+                { version: '2.1', supportedAppOs: 'Both', displayText: 'Core 2.1 (LTS)' },
+                { version: '3.1', supportedAppOs: 'Both', displayText: 'Core 3.1 (LTS)' },
+                { version: '5.0', supportedAppOs: 'Both', displayText: '5', buildMachineOsToSkip: 'darwin' } // Not sure why this fails on mac build machines - worth investigating in the future
             ]
         },
         {
             runtimePrefix: 'Python',
             workspaceFolder: 'python-docs-hello-world',
             versions: [
-                { version: '3.6', supportedOs: 'Both' },
-                { version: '3.7', supportedOs: 'Linux' },
-                { version: '3.8', supportedOs: 'Linux' }
+                { version: '3.6', supportedAppOs: 'Both', appOsToSkip: 'Windows' }, // Python on Windows has been deprecated for a while now, so not worth testing
+                { version: '3.7', supportedAppOs: 'Linux' },
+                { version: '3.8', supportedAppOs: 'Linux' }
             ]
         }
     ];
@@ -71,13 +72,12 @@ suite('Create Web App and deploy', async function (this: Mocha.Suite): Promise<v
     for (const testCase of testCases) {
         for (const version of testCase.versions) {
             // tslint:disable-next-line: strict-boolean-expressions
-            const runtime: string = `${testCase.runtimePrefix} ${version.version}${testCase.runtimeSuffix || ''}`;
-            const promptForOs: boolean = version.supportedOs === 'Both';
-            const oss: string[] = promptForOs ? ['Windows', 'Linux'] : [version.supportedOs];
+            const runtime: string = `${testCase.runtimePrefix} ${version.displayText || version.version}`;
+            const promptForOs: boolean = version.supportedAppOs === 'Both';
+            const oss: string[] = promptForOs ? ['Windows', 'Linux'] : [version.supportedAppOs];
             for (const os of oss) {
                 test(`${runtime} - ${os}`, async function (this: Mocha.Context): Promise<void> {
-                    if (testCase.runtimePrefix === 'Python' && os === 'Windows') {
-                        // Python on Windows has been deprecated for a while now, so not worth testing
+                    if (version.appOsToSkip === os || version.buildMachineOsToSkip === process.platform) {
                         this.skip();
                     }
 
@@ -115,7 +115,7 @@ suite('Create Web App and deploy', async function (this: Mocha.Suite): Promise<v
         assert.ok(createdApp);
 
         // Verify that the deployment is successful
-        await testUserInput.runWithInputs([workspacePath, resourceName, 'Deploy', DialogResponses.skipForNow.title], async () => {
+        await testUserInput.runWithInputs([workspacePath, resourceName, 'Deploy'], async () => {
             await vscode.commands.executeCommand('appService.Deploy');
         });
         const hostUrl: string | undefined = (<WebAppTreeItem>await ext.tree.findTreeItem(<string>createdApp?.id, context)).root.client.defaultHostUrl;
---
+++ test/nightly/testFolder/dotnet-hello-world/5.0/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _5._0
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    // Doesn't seem to be an easy way to get the version dynamically
+                    // Plus this should fail on any other framework anyways because of the TargetFramework in the csproj
+                    await context.Response.WriteAsync("Version: 5.0");
+                });
+            });
+        }
+    }
+}
