fa143ff27c9f0897ad5a0dacb4161e111bd394bd
Jason Zhai <v-wuzhai@microsoft.com>
2021-03-18 01:32:56 -0700

    Remove extra spaces in getRotatingValue.ts file

--- test/nightly/getRotatingValue.ts
+++ test/nightly/getRotatingValue.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-
 let locationCount: number = -1;
 const locationItem: string[] = ['Australia East', 'Australia Southeast', 'Brazil South', 'Canada Central', 'Central US', 'East Asia', 'East US', 'East US 2', 'France Central', 'Japan East', 'Japan West', 'Korea Central', 'Korea South', 'North Europe', 'South Central US', 'Southeast Asia', 'UK South', 'West Europe', 'West US', 'West US 2'];
 new Date().getDate() % 2 === 0 ? locationItem : locationItem.reverse();
