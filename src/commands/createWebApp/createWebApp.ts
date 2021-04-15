c55cdb5830aa072fc9fded372d14a243b5291bb1
nepothar <nepothar@microsoft.com>
2021-04-15 13:58:52 -0700

    fixed 403 error with Create...(Advanced) prompt

--- src/commands/createWebApp/createWebApp.ts
+++ src/commands/createWebApp/createWebApp.ts
@@ -3,24 +3,46 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureParentTreeItem, IActionContext, ICreateChildImplContext } from "vscode-azureextensionui";
+import { MessageItem, window } from "vscode";
+import { AzureParentTreeItem, callWithTelemetryAndErrorHandling, IActionContext, ICreateChildImplContext, parseError } from "vscode-azureextensionui";
 import { SubscriptionTreeItem } from '../../explorer/SubscriptionTreeItem';
 import { WebAppTreeItem } from "../../explorer/WebAppTreeItem";
 import { ext } from "../../extensionVariables";
+import { localize } from "../../localize";
 import { showCreatedWebAppMessage } from "./showCreatedWebAppMessage";
 
-export async function createWebApp(context: IActionContext & Partial<ICreateChildImplContext>, node?: AzureParentTreeItem | undefined, suppressCreatedWebAppMessage: boolean = false): Promise<WebAppTreeItem> {
+export async function createWebApp(context: IActionContext & Partial<ICreateChildImplContext>, node?: AzureParentTreeItem | undefined, suppressCreatedWebAppMessage: boolean = false): Promise<WebAppTreeItem | undefined> {
     if (!node) {
         node = <AzureParentTreeItem>await ext.tree.showTreeItemPicker(SubscriptionTreeItem.contextValue, context);
     }
-
-    const newSite: WebAppTreeItem = <WebAppTreeItem>await node.createChild(context);
-    if (!suppressCreatedWebAppMessage) {
-        showCreatedWebAppMessage(newSite);
+    let newSite: WebAppTreeItem;
+    try {
+        newSite = <WebAppTreeItem>await node.createChild(context);
+        if (!suppressCreatedWebAppMessage) {
+            showCreatedWebAppMessage(newSite);
+        }
+        return newSite;
+    } catch (error) {
+        if (!parseError(error).message.includes('does not have authorization to perform action')) {
+            throw error;
+        } else {
+            const message = localize('notAuthorizedToCreateRGs', 'You subscription is not authorized to create resource groups. Please create using Advanced Create to select existing resource groups.')
+            const advancedCreate: MessageItem = { title: localize('createAdvanced', 'Create - Advanced') };
+            // don't wait
+            void window.showInformationMessage(message, advancedCreate).then(async (result: MessageItem | undefined) => {
+                await callWithTelemetryAndErrorHandling('postCreateWebApp', async (context: IActionContext) => {
+                    context.telemetry.properties.dialogResult = result?.title;
+                    if (result) {
+                        newSite = <WebAppTreeItem>await createWebAppAdvanced(context, node);
+                        return newSite;
+                    }
+                });
+            });
+        }
     }
-    return newSite;
+
 }
 
-export async function createWebAppAdvanced(context: IActionContext, node?: AzureParentTreeItem | undefined): Promise<WebAppTreeItem> {
+export async function createWebAppAdvanced(context: IActionContext, node?: AzureParentTreeItem | undefined): Promise<WebAppTreeItem | undefined> {
     return await createWebApp({ ...context, advancedCreation: true }, node);
 }
