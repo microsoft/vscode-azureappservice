c9e6836c6dcb6840ce1018fbf38b8ee8c5d70641
Mingliang Wang <wangmi@microsoft.com>
2021-06-03 11:31:12 +0800

    add maven package task when user try to deploy a maven module to Azure.

---
+++ src/commands/deploy/setPreDeployTaskForMavenModule.ts
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import * as path from 'path';
+import { TaskDefinition } from 'vscode';
+import { IDeployContext } from 'vscode-azureappservice';
+import * as constants from '../../constants';
+import { javaUtils } from "../../utils/javaUtils";
+import { getWorkspaceSetting, updateWorkspaceSetting } from '../../vsCodeConfig/settings';
+import * as tasks from '../../vsCodeConfig/tasks';
+
+export async function setPreDeployTaskForMavenModule(context: IDeployContext, artifactId: string): Promise<void> {
+    const preDeployTaskKey: string = 'preDeployTask';
+    const workspaceFspath: string = context.workspaceFolder.uri.fsPath;
+    const mavenPackageTaskName: string = `package:${artifactId}`;
+
+    const preDeployTask = getWorkspaceSetting<string>(preDeployTaskKey, workspaceFspath);
+    if (!javaUtils.isMavenModule(context.effectiveDeployFsPath)) {
+        if (preDeployTask == mavenPackageTaskName) {
+            await updateWorkspaceSetting(preDeployTaskKey, undefined, workspaceFspath);
+        }
+        return;
+    } else if (preDeployTask) {
+        return;
+    }
+
+    const existingTasks: tasks.ITask[] = tasks.getTasks(context.workspaceFolder);
+    let packageTask: tasks.ITask | undefined = existingTasks.find(t1 => t1.label === mavenPackageTaskName);
+    // if the "package" task exists and it doesn't dependOn a task, have it depend on clean
+    if (!packageTask) {
+        packageTask = await generateMavenPackageTask(context.effectiveDeployFsPath, mavenPackageTaskName);
+        await tasks.updateTasks(context.workspaceFolder, existingTasks.concat([packageTask]));
+    }
+
+    const currentVersion: string | undefined = tasks.getTasksVersion(context.workspaceFolder);
+    if (!currentVersion) {
+        await tasks.updateTasksVersion(context.workspaceFolder, tasks.tasksVersion);
+    }
+
+    await updateWorkspaceSetting(preDeployTaskKey, mavenPackageTaskName, workspaceFspath);
+}
+
+async function generateMavenPackageTask(modulePath: string, taskName: string): Promise<TaskDefinition> {
+    const mvnWrapperName = constants.isWindows ? "./mvnw.cmd" : "./mvnw";
+    const mvnWrapper = await javaUtils.getLocalMavenWrapper(modulePath);
+    const cmd = mvnWrapper ? mvnWrapperName : 'mvn';
+    const cwd = mvnWrapper ? path.dirname(mvnWrapper) : modulePath;
+    const relativeModulePath: string = path.dirname(path.relative(cwd, modulePath));
+    const pomPath: string = path.posix.join(relativeModulePath, 'pom.xml');
+
+    return {
+        label: taskName,
+        command: cmd,
+        type: "shell",
+        group: "build",
+        args: ["clean", "package", "-f", pomPath],
+        options: { cwd },
+        presentation: {
+            echo: true,
+            reveal: "always",
+            focus: false,
+            panel: "shared",
+            showReuseMessage: true,
+            clear: false
+        },
+        problemMatcher: "$msCompile",
+    };
+}
--- src/utils/javaUtils.ts
+++ src/utils/javaUtils.ts
@@ -4,10 +4,15 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { WebSiteManagementModels } from "@azure/arm-appservice";
+import { SiteConfigResource } from "@azure/arm-appservice/esm/models";
+import * as parser from 'fast-xml-parser';
 import * as fse from 'fs-extra';
+import * as JSZip from "jszip";
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { workspace } from "vscode";
 import { UserCancelledError } from 'vscode-azureextensionui';
+import * as constants from "../constants";
 import { ext } from '../extensionVariables';
 import { localize } from "../localize";
 import { SiteTreeItem } from "../tree/SiteTreeItem";
@@ -15,31 +20,23 @@ import { SiteTreeItem } from "../tree/SiteTreeItem";
 export namespace javaUtils {
     const DEFAULT_PORT: string = '8080';
     const PORT_KEY: string = 'PORT';
+    const JAVA_WEB_CONTAINER_REGEX: RegExp = /^(tomcat|wildfly|jboss)/i;
+    const JAVA_SE_REGEX: RegExp = /^java/i;
 
-    function isJavaWebContainerRuntime(runtime: string | undefined): boolean {
-        return !!runtime && /^(tomcat|wildfly)/i.test(runtime);
+    function isJavaWebContainerRuntime(runtime: SiteConfigResource | undefined): boolean {
+        return (!!runtime?.javaContainer && (JAVA_WEB_CONTAINER_REGEX.test(runtime.javaContainer)) || (!!runtime?.linuxFxVersion && JAVA_WEB_CONTAINER_REGEX.test(runtime.linuxFxVersion)));
     }
 
-    function isJavaSERuntime(runtime: string | undefined): boolean {
-        return !!runtime && /^java/i.test(runtime);
+    function isJavaSERuntime(runtime: SiteConfigResource | undefined): boolean {
+        return (!!runtime?.javaContainer && (JAVA_SE_REGEX.test(runtime.javaContainer)) || (!!runtime?.linuxFxVersion && JAVA_SE_REGEX.test(runtime.linuxFxVersion)));
     }
 
-    export function isJavaRuntime(runtime: string | undefined): boolean {
-        return isJavaWebContainerRuntime(runtime) || isJavaSERuntime(runtime);
+    export function isJavaRuntime(siteConfig: SiteConfigResource | undefined): boolean {
+        return isJavaWebContainerRuntime(siteConfig) || isJavaSERuntime(siteConfig);
     }
 
     function isJavaArtifact(artifactPath: string): boolean {
         return /^(.jar|.war)/i.test(path.extname(artifactPath));
-    }
-
-    function getArtifactTypeByJavaRuntime(runtime: string | undefined): string {
-        if (isJavaSERuntime(runtime)) {
-            return 'jar';
-        } else if (isJavaWebContainerRuntime(runtime)) {
-            return 'war';
-        } else {
-            throw new Error(localize('invalidJava', 'Invalid java runtime: {0}', runtime));
-        }
     }
 
     function getJavaArtifactExtensions(): string[] {
@@ -110,14 +107,66 @@ export namespace javaUtils {
         return node.root.client.updateApplicationSettings(appSettings);
     }
 
-    export async function getJavaFileExtensions(siteConfig: WebSiteManagementModels.SiteConfigResource | undefined): Promise<string | string[] | undefined> {
-        if (siteConfig && isJavaRuntime(siteConfig.linuxFxVersion)) {
-            return getArtifactTypeByJavaRuntime(siteConfig.linuxFxVersion);
+    export async function getJavaFileExtensions(siteConfig: WebSiteManagementModels.SiteConfigResource | undefined): Promise<string[] | undefined> {
+        if (isJavaSERuntime(siteConfig)) {
+            return ['jar'];
+        } else if (isJavaWebContainerRuntime(siteConfig)) {
+            return ['war'];
         } else if (await isJavaProject()) {
             return getJavaArtifactExtensions();
         }
-
         return;
+    }
+
+    export function getMavenModule(module: string): { artifactId: string, packaging: string } | undefined {
+        if (!isMavenModule(module)) {
+            return undefined;
+        }
+        const pomFile: string = path.join(module, 'pom.xml');
+        const pomContent = fse.readFileSync(pomFile, 'utf8');
+        const pom = parser.parse(pomContent) as { project: { artifactId: string, packaging: string } };
+        return {
+            artifactId: pom.project.artifactId,
+            packaging: pom.project.packaging || 'jar'
+        } as { artifactId: string, packaging: string };
+    }
+
+    export function isMavenModule(fsPath: string): boolean {
+        return fse.existsSync(fsPath) &&
+            fse.lstatSync(fsPath).isDirectory() &&
+            fse.existsSync(path.join(fsPath, 'pom.xml'));
+    }
+
+    export async function getLocalMavenWrapper(modulePath: string): Promise<string | undefined> {
+        const mvnw: string = constants.isWindows ? "mvnw.cmd" : "mvnw";
+        // walk up parent folders
+        let current: string = modulePath;
+        while (path.basename(current)) {
+            const potentialMvnwPath: string = path.join(current, mvnw);
+            if (await fse.pathExists(potentialMvnwPath)) {
+                return potentialMvnwPath;
+            }
+            current = path.dirname(current);
+        }
+        return undefined;
+    }
+
+    export async function isExecutableJarFile(fsPath: string): Promise<boolean> {
+        const fileContent: Buffer = await fse.readFile(fsPath);
+        const zip: JSZip = await JSZip.loadAsync(fileContent);
+        const manifest: string | undefined = await zip.file("META-INF/MANIFEST.MF")?.async("text");
+        return !!(manifest && manifest.toLocaleLowerCase().includes("main-class"));
+    }
+
+    export async function getExecutableArtifactsByExtensions(targetFolder: string, fileExtensions: string[] | undefined): Promise<string[] | undefined> {
+        if (!fileExtensions || !targetFolder || !fse.pathExistsSync(targetFolder)) {
+            return undefined;
+        }
+        return (await Promise.all(fileExtensions.map(async ext => {
+            const relativeDirectory: vscode.RelativePattern | string = new vscode.RelativePattern(targetFolder, `**/*.${ext}`);
+            const uris = await vscode.workspace.findFiles(relativeDirectory);
+            return uris.map(u => u.fsPath).filter(async u => ext.toLowerCase() !== 'jar' || await isExecutableJarFile(u));
+        }))).reduce((acc, val) => acc.concat(val), []);
     }
 
 }
