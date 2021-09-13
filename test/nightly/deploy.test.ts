74d182ed01ba4fa7f2354f52219c1365c0bc29dd
v-wuzhai <46013274+v-wuzhai@users.noreply.github.com>
2021-09-14 04:40:07 +0800

    Add deploying .NET 6.0 project test case and remove .NET 2.1 test case (#2125)

--- test/nightly/deploy.test.ts
+++ test/nightly/deploy.test.ts
@@ -56,9 +56,9 @@ suite('Create Web App and deploy', function (this: Mocha.Suite): void {
             runtimePrefix: '.NET',
             workspaceFolder: undefined,
             versions: [
-                { version: '2.1', supportedAppOs: 'Both', displayText: 'Core 2.1 (LTS)' },
                 { version: '3.1', supportedAppOs: 'Both', displayText: 'Core 3.1 (LTS)' },
-                { version: '5.0', supportedAppOs: 'Both', displayText: '5', buildMachineOsToSkip: 'darwin' } // Not sure why this fails on mac build machines - worth investigating in the future
+                { version: '5.0', supportedAppOs: 'Both', displayText: '5', buildMachineOsToSkip: 'darwin' }, // Not sure why this fails on mac build machines - worth investigating in the future
+                { version: '6.0', supportedAppOs: 'Both', displayText: '6', buildMachineOsToSkip: 'darwin' }
             ]
         },
         {
