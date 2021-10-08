b29c65dc13d9ecbe10e8a2f0d353ab0eaa45b55d
v-wuzhai <46013274+v-wuzhai@users.noreply.github.com>
2021-10-08 02:32:08 -0700

    Fix .NET6 nightly test project (#2139)
    * Fix .NET6 nightly test project
    
    * Update the .net6 test project

--- test/nightly/testFolder/dotnet-hello-world/6.0/Program.cs
+++ test/nightly/testFolder/dotnet-hello-world/6.0/Program.cs
@@ -1,11 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
 app.MapGet("/", () => "Version: 6.0");
 
 app.Run();
