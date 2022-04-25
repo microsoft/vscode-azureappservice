19ee67ce12ae800776119f77d9e19eb240743b03
Alex Weininger <alex.weininger@live.com>
2022-04-25 16:01:12 -0700

    Fix open in portal

--- src/commands/openInPortal.ts
+++ src/commands/openInPortal.ts
@@ -6,29 +6,38 @@
 import { DeploymentsTreeItem } from "@microsoft/vscode-azext-azureappservice";
 import { openInPortal as uiOpenInPortal } from '@microsoft/vscode-azext-azureutils';
 import { AzExtTreeItem, IActionContext } from "@microsoft/vscode-azext-utils";
-import { ext } from "../extensionVariables";
 import { DeploymentSlotsTreeItem } from "../tree/DeploymentSlotsTreeItem";
-import { ResolvedWebAppResource } from "../tree/ResolvedWebAppResource";
-import { SiteTreeItem } from "../tree/SiteTreeItem";
 import { nonNullProp } from "../utils/nonNull";
 
-export async function openInPortal(context: IActionContext, node?: AzExtTreeItem): Promise<void> {
-    if (!node) {
-        node = await ext.rgApi.tree.showTreeItemPicker<SiteTreeItem>(new RegExp(ResolvedWebAppResource.webAppContextValue), context);
+export async function openInPortal(context: IActionContext, node: AzExtTreeItem): Promise<void> {
+    if (matchContextValue(node.contextValue, [DeploymentSlotsTreeItem.contextValue])) {
+        // the deep link for slots does not follow the conventional pattern of including its parent in the path name so this is how we extract the slot's id
+        await uiOpenInPortal(node, `${nonNullProp(node, 'parent').id}/deploymentSlotsV2`);
+        return;
     }
 
-    switch (node.contextValue) {
-        // the deep link for slots does not follow the conventional pattern of including its parent in the path name so this is how we extract the slot's id
-        case DeploymentSlotsTreeItem.contextValue:
-            await uiOpenInPortal(node, `${nonNullProp(node, 'parent').fullId}/deploymentSlotsV2`);
-            return;
+    if (matchContextValue(node.contextValue, [new RegExp(DeploymentsTreeItem.contextValueConnected), new RegExp(DeploymentsTreeItem.contextValueUnconnected)])) {
         // the deep link for "Deployments" do not follow the conventional pattern of including its parent in the path name so we need to pass the "Deployment Center" url directly
-        case DeploymentsTreeItem.contextValueConnected:
-        case DeploymentsTreeItem.contextValueUnconnected:
-            await uiOpenInPortal(node, `${nonNullProp(node, 'parent').fullId}/vstscd`);
-            return;
-        default:
-            await uiOpenInPortal(node, node.fullId);
-            return;
+        const id = `${nonNullProp(node, 'parent').id}/vstscd`;
+        await uiOpenInPortal(node, id);
+        return;
+    }
+}
+
+function matchContextValue(expectedContextValue: RegExp | string, matches: (string | RegExp)[]): boolean {
+    if (expectedContextValue instanceof RegExp) {
+        return matches.some((match) => {
+            if (match instanceof RegExp) {
+                return expectedContextValue.toString() === match.toString();
+            }
+            return expectedContextValue.test(match);
+        });
+    } else {
+        return matches.some((match) => {
+            if (match instanceof RegExp) {
+                return match.test(expectedContextValue);
+            }
+            return expectedContextValue === match;
+        });
     }
 }
