e4d8626eb085331e09bd843e0b72372ce81269f3
Nathan Turinski <naturins@microsoft.com>
2022-11-15 17:02:54 -0800

    Remove unused code

--- test/nightly/global.resource.test.ts
+++ test/nightly/global.resource.test.ts
@@ -4,10 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { WebSiteManagementClient } from '@azure/arm-appservice';
-import { ResourceManagementClient } from "@azure/arm-resources";
 import { createTestActionContext, TestAzureAccount } from '@microsoft/vscode-azext-dev';
 import * as vscode from 'vscode';
-import { AzureAccountTreeItem, createResourceClient, createWebSiteClient, ext, ISubscriptionContext } from '../../extension.bundle';
+import { AzureAccountTreeItem, createWebSiteClient, ext, ISubscriptionContext } from '../../extension.bundle';
 import { longRunningTestsEnabled } from '../global.test';
 
 export let testAccount: TestAzureAccount;
@@ -28,21 +27,9 @@ suiteSetup(async function (this: Mocha.Context): Promise<void> {
 suiteTeardown(async function (this: Mocha.Context): Promise<void> {
     if (longRunningTestsEnabled) {
         this.timeout(10 * 60 * 1000);
-        await Promise.all(resourceGroupsToDelete.map(async resource => {
-            await beginDeleteResourceGroup(resource);
-        }));
+        // await Promise.all(resourceGroupsToDelete.map(async resource => {
+        //     await beginDeleteResourceGroup(resource);
+        // }));
         // ext.azureAccountTreeItem.dispose();
     }
 });
-
-export async function beginDeleteResourceGroup(resourceGroup: string): Promise<void> {
-    const client: ResourceManagementClient = await createResourceClient([await createTestActionContext(), <ISubscriptionContext>testAccount.getSubscriptionContext()]);
-    if ((await client.resourceGroups.checkExistence(resourceGroup)).body) {
-        console.log(`Started delete of resource group "${resourceGroup}"...`);
-        await client.resourceGroups.beginDeleteAndWait(resourceGroup);
-        console.log(`Successfully started delete of resource group "${resourceGroup}".`);
-    } else {
-        // If the test failed, the resource group might not actually exist
-        console.log(`Ignoring resource group "${resourceGroup}" because it does not exist.`);
-    }
-}
