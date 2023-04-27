371632af2e650cb4e6556a4d7ab3099e37ae5ff4
Nathan Turinski <naturins@microsoft.com>
2023-04-26 22:18:12 -0700

    Merge old stash

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@
 
 const process = require('process');
 const dev = require("@microsoft/vscode-azext-dev");
+<<<<<<< Updated upstream
+=======
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+>>>>>>> Stashed changes
 
 let DEBUG_WEBPACK = !/^(false|0)?$/i.test(process.env.DEBUG_WEBPACK || '');
 
@@ -27,6 +34,44 @@ let config = dev.getDefaultWebpackConfig({
         '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
         '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
     },
+    target: 'node',
+    suppressCleanDistFolder: true
+});
+
+let webConfig = dev.getDefaultWebpackConfig({
+    projectRoot: __dirname,
+    verbosity: DEBUG_WEBPACK ? 'debug' : 'normal',
+    externals:
+    {
+        // Fix "Module not found" errors in ./node_modules/websocket/lib/{BufferUtil,Validation}.js
+        // These files are not in node_modules and so will fail normally at runtime and instead use fallbacks.
+        // Make them as external so webpack doesn't try to process them, and they'll simply fail at runtime as before.
+        '../build/Release/validation': 'commonjs ../build/Release/validation',
+        '../build/default/validation': 'commonjs ../build/default/validation',
+        '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
+        '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
+    },
+    target: 'webworker',
+    entries: { polyfill: './src/polyfill.worker.ts', },
+    plugins: [new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+    new HtmlWebpackPlugin({
+        title: 'Storage Web Worker Sample',
+    }),
+    new NodePolyfillPlugin({
+        includeAliases: [
+            "path", "stream", "process", "Buffer"
+        ]
+    })],
+    suppressCleanDistFolder: true,
+    resolveFallbackAliases: {
+        "timers": require.resolve("timers-browserify"),
+        'constants': false,
+        'tls': false,
+        'bufferutil': false,
+        'utf-8-validate': false
+    }
 });
 
 if (DEBUG_WEBPACK) {
