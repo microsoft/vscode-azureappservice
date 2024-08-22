3e608f80a4168bf2f1780af98f1a536435f6cd3c
Nathan Turinski <naturins@microsoft.com>
2024-08-22 12:43:25 -0700

    Add a listTask so the cache works for all resolves

--- src/WebAppResolver.ts
+++ src/WebAppResolver.ts
@@ -9,18 +9,29 @@ export class WebAppResolver implements AppResourceResolver {
 
     private siteCacheLastUpdated = 0;
     private siteCache: Map<string, Site> = new Map<string, Site>();
+    private listWebAppsTask: Promise<void> | undefined;
 
     public async resolveResource(subContext: ISubscriptionContext, resource: AppResource): Promise<ResolvedWebAppResource | undefined> {
         return await callWithTelemetryAndErrorHandling('resolveResource', async (context: IActionContext) => {
             const client = await createWebSiteClient({ ...context, ...subContext });
 
             if (this.siteCacheLastUpdated < Date.now() - 1000 * 3) {
-                this.siteCache.clear();
-                const sites = await uiUtils.listAllIterator(client.webApps.list());
-                sites.forEach(site => this.siteCache.set(nonNullProp(site, 'id').toLowerCase(), site));
                 this.siteCacheLastUpdated = Date.now();
+                this.listWebAppsTask = new Promise((resolve, reject) => {
+                    this.siteCache.clear();
+                    uiUtils.listAllIterator(client.webApps.list()).then((sites) => {
+                        for (const site of sites) {
+                            this.siteCache.set(nonNullProp(site, 'id').toLowerCase(), site);
+                        }
+                        resolve();
+                    })
+                        .catch((reason) => {
+                            reject(reason);
+                        });
+                });
             }
 
+            await this.listWebAppsTask;
             const site = this.siteCache.get(nonNullProp(resource, 'id').toLowerCase());
             return new ResolvedWebAppResource(subContext, nonNullValue(site));
         });
