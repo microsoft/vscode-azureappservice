ae9d71c9eb9def1daf3ef8968c9d5408559c11de
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-02-03 12:09:00 -0800

    Add copyright header

--- src/commands/createWebApp/domainLabelScope/types.ts
+++ src/commands/createWebApp/domainLabelScope/types.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { type SiteConfig } from "@azure/arm-appservice";
 import { type DomainNameLabelScope } from "@microsoft/vscode-azext-azureappservice";
 
