70b31bf26c49bd1fec07d5cf48059235f3d2ed72
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-03-07 16:01:21 -0800

    Add var for api version

--- src/commands/createWebApp/WebAppCreateStep.ts
+++ src/commands/createWebApp/WebAppCreateStep.ts
@@ -86,9 +86,10 @@ export class WebAppCreateStep extends AzureWizardExecuteStep<IWebAppWizardContex
     // #region createNewSiteWithDomainLabelScope
     private async createNewSiteWithDomainLabelScope(context: IWebAppWizardContext, sdkClient: WebSiteManagementClient, rgName: string, siteName: string): Promise<Site> {
         // The SDK does not currently support this updated api version, so we should make the call to the endpoint manually until the SDK gets updated
+        const apiVersion: string = '2024-04-01';
         const authToken = (await context.credentials.getToken() as { token?: string }).token;
         const options: AzExtRequestPrepareOptions = {
-            url: `${context.environment.resourceManagerEndpointUrl}subscriptions/${context.subscriptionId}/resourceGroups/${rgName}/providers/Microsoft.Web/sites/${siteName}?api-version=2024-04-01`,
+            url: `${context.environment.resourceManagerEndpointUrl}subscriptions/${context.subscriptionId}/resourceGroups/${rgName}/providers/Microsoft.Web/sites/${siteName}?api-version=${apiVersion}`,
             method: 'PUT',
             headers: createHttpHeaders({
                 'Content-Type': 'application/json',
