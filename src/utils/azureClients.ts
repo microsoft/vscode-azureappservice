d79f8e0620ab13678379700a7f8bb679cde2a942
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2026-02-05 16:47:29 -0800

    Remove references to manual calls

--- src/utils/azureClients.ts
+++ src/utils/azureClients.ts
@@ -6,13 +6,13 @@
 import { type WebSiteManagementClient } from '@azure/arm-appservice';
 import { type ResourceGraphClient } from '@azure/arm-resourcegraph';
 import { type ResourceManagementClient } from '@azure/arm-resources';
-import { createAzureClient, createAzureSubscriptionClient, type AzExtClientContext } from '@microsoft/vscode-azext-azureutils';
+import { AzExtClientType, createAzureClient, createAzureSubscriptionClient, type AzExtClientContext } from '@microsoft/vscode-azext-azureutils';
 
 // Lazy-load @azure packages to improve startup performance.
 // NOTE: The client is the only import that matters, the rest of the types disappear when compiled to JavaScript
 
 export async function createWebSiteClient(clientContext: AzExtClientContext): Promise<WebSiteManagementClient> {
-    return createAzureClient(clientContext, (await import('@azure/arm-appservice')).WebSiteManagementClient);
+    return createAzureClient(clientContext, (await import('@azure/arm-appservice')).WebSiteManagementClient as unknown as AzExtClientType<WebSiteManagementClient>);
 }
 export async function createResourceClient(clientContext: AzExtClientContext): Promise<ResourceManagementClient> {
     return createAzureClient(clientContext, (await import('@azure/arm-resources')).ResourceManagementClient);
